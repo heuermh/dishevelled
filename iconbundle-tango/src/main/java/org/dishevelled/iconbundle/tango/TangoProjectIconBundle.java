809e4e15ae10eeca62f614931c798ae88bfc132d
heuermh <heuermh@acm.org>
2006-01-02 23:44:17 +0000

    updated copyright year

--- iconbundle-tango/src/main/java/org/dishevelled/iconbundle/tango/TangoProjectIconBundle.java
+++ iconbundle-tango/src/main/java/org/dishevelled/iconbundle/tango/TangoProjectIconBundle.java
@@ -1,7 +1,7 @@
 /*
 
     dsh-iconbundle-tango  Icon bundles for the Tango Project icon theme.
-    Copyright (c) 2005 held jointly by the individual authors.
+    Copyright (c) 2005-2006 held jointly by the individual authors.
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published
