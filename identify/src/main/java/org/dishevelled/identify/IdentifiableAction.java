e48f6132173c4db2549b14f874e24d0add64e572
heuermh <heuermh@acm.org>
2006-02-09 06:43:01 +0000

    adding first cut at IdButton

---
+++ identify/src/main/java/org/dishevelled/identify/IdentifiableAction.java
@@ -0,0 +1,86 @@
+/*
+
+    dsh-identify  Lightweight components for identifiable beans.
+    Copyright (c) 2003-2006 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.gnu.org/copyleft/lesser.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.identify;
+
+import javax.swing.AbstractAction;
+
+import org.dishevelled.iconbundle.IconBundle;
+
+/**
+ * An abstract identifiable action.
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public abstract class IdentifiableAction
+    extends AbstractAction
+    implements Identifiable
+{
+    /** Icon bundle. */
+    private IconBundle iconBundle;
+
+
+    /**
+     * Create a new abstract identifiable action with the specified name
+     * and icon bundle.
+     *
+     * @param name name for this identifiable action
+     * @param iconBundle icon bundle for this identifiable action, must
+     *    not be null
+     */
+    protected IdentifiableAction(final String name, final IconBundle iconBundle)
+    {
+        super(name);
+        setIconBundle(iconBundle);
+    }
+
+
+    /**
+     * Set the icon bundle for this identifiable action to <code>iconBundle</code>.
+     *
+     * @param iconBundle icon bundle for this identifiable action, must not be null
+     */
+    public final void setIconBundle(final IconBundle iconBundle)
+    {
+        if (iconBundle == null)
+        {
+            throw new IllegalArgumentException("iconBundle must not be null");
+        }
+        IconBundle oldIconBundle = this.iconBundle;
+        this.iconBundle = iconBundle;
+        firePropertyChange("iconBundle", oldIconBundle, this.iconBundle);
+    }
+
+    /** @see Identifiable */
+    public final String getName()
+    {
+        return (String) getValue(NAME);
+    }
+
+    /** @see Identifiable */
+    public final IconBundle getIconBundle()
+    {
+        return iconBundle;
+    }
+}
