43ae4564b67ce8ff970b71ba74ef62ec58d79fb9
heuermh <heuermh@acm.org>
2007-01-02 22:32:30 +0000

    updating copyright year

--- evolve/src/test/java/org/dishevelled/evolve/select/FitnessProportionalSelectionTest.java
+++ evolve/src/test/java/org/dishevelled/evolve/select/FitnessProportionalSelectionTest.java
@@ -1,7 +1,7 @@
 /*
 
     dsh-evolve  Simple framework for evolutionary algorithms.
-    Copyright (c) 2005 held jointly by the individual authors.
+    Copyright (c) 2005-2007 held jointly by the individual authors.
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published
