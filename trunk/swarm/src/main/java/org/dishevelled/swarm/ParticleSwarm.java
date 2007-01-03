d9b3fee1abc3bd010a08e5bada04e017beb4ed12
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2007-01-03 10:15:13 +0000

    creating memento class to hold particle swarm matrices

--- trunk/swarm/src/main/java/org/dishevelled/swarm/ParticleSwarm.java
+++ trunk/swarm/src/main/java/org/dishevelled/swarm/ParticleSwarm.java
@@ -23,20 +23,114 @@
 */
 package org.dishevelled.swarm;
 
+import org.dishevelled.matrix.ObjectMatrix1D;
+import org.dishevelled.matrix.ObjectMatrix2D;
+
 /**
- * Particle swarm.
+ * A memento class holding the particle position, particle velocity,
+ * cognitive memory, and social memory matrices for a particle swarm.
  *
  * @author  Michael Heuer
  * @version $Revision$ $Date$
  */
 final class ParticleSwarm
 {
-    // empty
+    /** Particle position matrix. */
+    private final ObjectMatrix2D<Double> position;
+
+    /** Particle velocity matrix. */
+    private final ObjectMatrix2D<Double> velocity;
+
+    /** Cognitive memory matrix. */
+    private final ObjectMatrix2D<Double> cognitiveMemory;
+
+    /** Social memory matrix. */
+    private final ObjectMatrix1D<Double> socialMemory;
+
+
+    /**
+     * Create a new particle swarm.
+     *
+     * @param position particle position matrix, must not be null
+     * @param velocity particle velocity matrix, must not be null
+     * @param cognitiveMemory cognitive memory matrix, must not be null
+     * @param socialMemory social memory matrix, must not be null
+     */
+    ParticleSwarm(final ObjectMatrix2D<Double> position,
+                  final ObjectMatrix2D<Double> velocity,
+                  final ObjectMatrix2D<Double> cognitiveMemory,
+                  final ObjectMatrix1D<Double> socialMemory)
+    {
+        if (position == null)
+        {
+            throw new IllegalArgumentException("position must not be null");
+        }
+        if (velocity == null)
+        {
+            throw new IllegalArgumentException("velocity must not be null");
+        }
+        if (cognitiveMemory == null)
+        {
+            throw new IllegalArgumentException("cognitiveMemory must not be null");
+        }
+        if (socialMemory == null)
+        {
+            throw new IllegalArgumentException("socialMemory must not be null");
+        }
+        this.position = position;
+        this.velocity = velocity;
+        this.cognitiveMemory = cognitiveMemory;
+        this.socialMemory = socialMemory;
+    }
+
+
+    /**
+     * Return the particle position matrix for this particle swarm.  The particle position
+     * matrix will not be null.
+     *
+     * @return the particle position matrix for this particle swarm
+     */
+    public ObjectMatrix2D<Double> getPosition()
+    {
+        return position;
+    }
+
+    /**
+     * Return the particle velocity matrix for this particle swarm.  The particle velocity
+     * matrix will not be null.
+     *
+     * @return the particle velocity matrix for this particle swarm
+     */
+    public ObjectMatrix2D<Double> getVelocity()
+    {
+        return velocity;
+    }
+
+    /**
+     * Return the cognitive memory matrix for this particle swarm.  The cognitive memory
+     * matrix will not be null.
+     *
+     * @return the cognitive memory matrix for this particle swarm
+     */
+    public ObjectMatrix2D<Double> getCognitiveMemory()
+    {
+        return cognitiveMemory;
+    }
+
+    /**
+     * Return the social memory matrix for this particle swarm.  The social memory
+     * matrix will not be null.
+     *
+     * @return the social memory matrix for this particle swarm
+     */
+    public ObjectMatrix1D<Double> getSocialMemory()
+    {
+        return socialMemory;
+    }
 
     // todo:
-    //   - optimize returns ParticleSwarm?
+    //   - optimize returns ParticleSwarm
     //   - exit strategy --> evaluate(ParticleSwarm, epoch)
     //   - fitness --> score(ParticleSwarm, ...)
-    //   - ParticleSwarm should allow public read-only access to data
-    //       and package level read/write access
+    //   - matrices should be unmodifiable, on access
 }
