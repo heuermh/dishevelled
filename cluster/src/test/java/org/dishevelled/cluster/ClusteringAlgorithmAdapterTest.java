5c0656967b8e5bb49bf1fbfa8bde753885494156
heuermh <heuermh@acm.org>
2007-04-17 03:16:39 +0000

    moving cluster-->clustering

---
+++ cluster/src/test/java/org/dishevelled/cluster/ClusteringAlgorithmAdapterTest.java
@@ -0,0 +1,75 @@
+/*
+
+    dsh-cluster  Framework for clustering algorithms.
+    Copyright (c) 2007 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.gnu.org/copyleft/lesser.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.cluster;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for ClusteringAlgorithmAdapter.
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public final class ClusteringAlgorithmAdapterTest
+    extends TestCase
+{
+
+    public void testConstructor()
+    {
+        ClusteringAlgorithmAdapter<String> adapter0 = new ClusteringAlgorithmAdapter<String>();
+    }
+
+    public void testExitFailed()
+    {
+        ClusteringAlgorithmAdapter<String> adapter = new ClusteringAlgorithmAdapter<String>();
+        adapter.exitFailed(new ClusteringAlgorithmEvent<String>(new TestClusteringAlgorithm<String>()));
+    }
+
+    public void testExitSucceeded()
+    {
+        ClusteringAlgorithmAdapter<String> adapter = new ClusteringAlgorithmAdapter<String>();
+        adapter.exitSucceeded(new ClusteringAlgorithmEvent<String>(new TestClusteringAlgorithm<String>()));
+    }
+
+    /**
+     * Test clustering algorithm.
+     */
+    private class TestClusteringAlgorithm<E>
+        extends AbstractClusteringAlgorithm<E>
+    {
+
+        /** {@inheritDoc} */
+        public Set<Cluster<E>> cluster(final List<? extends E> values,
+                                       final Similarity<? super E> similarity,
+                                       final ExitStrategy<? super E> exitStrategy)
+            throws ClusteringAlgorithmException
+        {
+            return Collections.<Cluster<E>>emptySet();
+        }
+    }
+}
