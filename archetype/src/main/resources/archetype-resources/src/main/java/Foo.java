d58eb363802e7f07c0b356a9fcacb5b9d3335ae4
heuermh <heuermh@acm.org>
2008-01-02 19:14:54 +0000

    updating copyright years to 2008

--- archetype/src/main/resources/archetype-resources/src/main/java/Foo.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/Foo.java
@@ -1,7 +1,7 @@
 /*
 
     ${artifactId}  ${description}
-    Copyright (c) 2007 held jointly by the individual authors.
+    Copyright (c) 2008 held jointly by the individual authors.
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published
