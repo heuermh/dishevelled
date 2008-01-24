2025d82d66699e1242d4aae222631cd027789524
heuermh <heuermh@acm.org>
2008-01-24 23:23:44 +0000

    replaced colt StaticBin1D with apache commons-math SummaryStatistics; updated special cases doc, size method return value, and unit tests accordingly

--- timer/src/test/java/org/dishevelled/timer/TimerTest.java
+++ timer/src/test/java/org/dishevelled/timer/TimerTest.java
@@ -46,18 +46,18 @@ public class TimerTest
     {
         Timer t = new Timer();
         assertNotNull("t not null", t);
-        assertEquals("t size == 0", 0, t.size());
-        assertEquals("t min == Double.POSITIVE_INFINITY", Double.POSITIVE_INFINITY, t.min());
-        assertEquals("t max == Double.NEGATIVE_INFINITY", Double.NEGATIVE_INFINITY, t.max());
-        assertEquals("t sum == 0.0d", 0.0d, t.sum());
+        assertEquals("t size == 0", 0L, t.size());
+        assertEquals("t min == Double.NaN", Double.NaN, t.min());
+        assertEquals("t max == Double.NaN", Double.NaN, t.max());
+        assertEquals("t sum == Double.NaN", Double.NaN, t.sum());
         assertEquals("t mean == Double.NaN", Double.NaN, t.mean());
         assertEquals("t standardDeviation == Double.NaN", Double.NaN, t.standardDeviation());
 
         t.reset();
-        assertEquals("t size == 0", 0, t.size());
-        assertEquals("t min == Double.POSITIVE_INFINITY", Double.POSITIVE_INFINITY, t.min());
-        assertEquals("t max == Double.NEGATIVE_INFINITY", Double.NEGATIVE_INFINITY, t.max());
-        assertEquals("t sum == 0.0d", 0.0d, t.sum());
+        assertEquals("t size == 0", 0L, t.size());
+        assertEquals("t min == Double.NaN", Double.NaN, t.min());
+        assertEquals("t max == Double.NaN", Double.NaN, t.max());
+        assertEquals("t sum == Double.NaN", Double.NaN, t.sum());
         assertEquals("t mean == Double.NaN", Double.NaN, t.mean());
         assertEquals("t standardDeviation == Double.NaN", Double.NaN, t.standardDeviation());
 
@@ -73,18 +73,18 @@ public class TimerTest
         }
 
         t.stop();
-        assertEquals("t size == 1", 1, t.size());
+        assertEquals("t size == 1", 1L, t.size());
         assertTrue("t min > 0.0d", t.min() > 0.0d);
         assertTrue("t max > 0.0d", t.max() > 0.0d);
         assertTrue("t sum > 0.0d", t.sum() > 0.0d);
         assertTrue("t mean > 0.0d", t.mean() > 0.0d);
-        assertEquals("t standardDeviation == Double.NaN", Double.NaN, t.standardDeviation());
+        assertEquals("t standardDeviation == 0.0d", 0.0d, t.standardDeviation());
 
         t.reset();
-        assertEquals("t size == 0", 0, t.size());
-        assertEquals("t min == Double.POSITIVE_INFINITY", Double.POSITIVE_INFINITY, t.min());
-        assertEquals("t max == Double.NEGATIVE_INFINITY", Double.NEGATIVE_INFINITY, t.max());
-        assertEquals("t sum == 0.0d", 0.0d, t.sum());
+        assertEquals("t size == 0", 0L, t.size());
+        assertEquals("t min == Double.NaN", Double.NaN, t.min());
+        assertEquals("t max == Double.NaN", Double.NaN, t.max());
+        assertEquals("t sum == Double.NaN", Double.NaN, t.sum());
         assertEquals("t mean == Double.NaN", Double.NaN, t.mean());
         assertEquals("t standardDeviation == Double.NaN", Double.NaN, t.standardDeviation());
 
@@ -153,7 +153,7 @@ public class TimerTest
 
         t.stop();
 
-        assertEquals("t size == 5", 5, t.size());
+        assertEquals("t size == 5", 5L, t.size());
         assertTrue("t min > 0.0d", t.min() > 0.0d);
         assertTrue("t max > 0.0d", t.max() > 0.0d);
         assertTrue("t sum > 0.0d", t.sum() > 0.0d);
@@ -161,10 +161,10 @@ public class TimerTest
         assertTrue("t standardDeviation > 0.0d", t.standardDeviation() > 0.0d);
 
         t.reset();
-        assertEquals("t size == 0", 0, t.size());
-        assertEquals("t min == Double.POSITIVE_INFINITY", Double.POSITIVE_INFINITY, t.min());
-        assertEquals("t max == Double.NEGATIVE_INFINITY", Double.NEGATIVE_INFINITY, t.max());
-        assertEquals("t sum == 0.0d", 0.0d, t.sum());
+        assertEquals("t size == 0", 0L, t.size());
+        assertEquals("t min == Double.NaN", Double.NaN, t.min());
+        assertEquals("t max == Double.NaN", Double.NaN, t.max());
+        assertEquals("t sum == Double.NaN", Double.NaN, t.sum());
         assertEquals("t mean == Double.NaN", Double.NaN, t.mean());
         assertEquals("t standardDeviation == Double.NaN", Double.NaN, t.standardDeviation());
     }
@@ -176,19 +176,19 @@ public class TimerTest
         t.stop();
         t.stop();
         t.stop();
-        assertEquals("t size == 3", 3, t.size());
+        assertEquals("t size == 3", 3L, t.size());
 
         t.start();
         t.stop();
         t.stop();
-        assertEquals("t size == 5", 5, t.size());
+        assertEquals("t size == 5", 5L, t.size());
 
         t.reset();
         t.start();
         t.stop();
         t.stop();
         t.stop();
-        assertEquals("t size == 3", 3, t.size());
+        assertEquals("t size == 3", 3L, t.size());
     }
 
     public void testTimerException()
@@ -295,46 +295,46 @@ public class TimerTest
         assertNotNull(t1);
         assertNotNull(t2);
 
-        assertEquals("t0 size == 1", 1, t0.size());
+        assertEquals("t0 size == 1", 1L, t0.size());
         assertTrue("t0 min > 0.0d", t0.min() > 0.0d);
         assertTrue("t0 max > 0.0d", t0.max() > 0.0d);
         assertTrue("t0 sum > 0.0d", t0.sum() > 0.0d);
         assertTrue("t0 mean > 0.0d", t0.mean() > 0.0d);
-        assertEquals("t0 standardDeviation == Double.NaN", Double.NaN, t0.standardDeviation());
+        assertEquals("t0 standardDeviation == 0.0d", 0.0d, t0.standardDeviation());
 
-        assertEquals("t1 size == 1", 1, t1.size());
+        assertEquals("t1 size == 1", 1L, t1.size());
         assertTrue("t1 min > 0.0d", t1.min() > 0.0d);
         assertTrue("t1 max > 0.0d", t1.max() > 0.0d);
         assertTrue("t1 sum > 0.0d", t1.sum() > 0.0d);
         assertTrue("t1 mean > 0.0d", t1.mean() > 0.0d);
-        assertEquals("t1 standardDeviation == Double.NaN", Double.NaN, t1.standardDeviation());
+        assertEquals("t1 standardDeviation == 0.0d", 0.0d, t1.standardDeviation());
 
-        assertEquals("t2 size == 1", 1, t2.size());
+        assertEquals("t2 size == 1", 1L, t2.size());
         assertTrue("t2 min > 0.0d", t2.min() > 0.0d);
         assertTrue("t2 max > 0.0d", t2.max() > 0.0d);
         assertTrue("t2 sum > 0.0d", t2.sum() > 0.0d);
         assertTrue("t2 mean > 0.0d", t2.mean() > 0.0d);
-        assertEquals("t2 standardDeviation == Double.NaN", Double.NaN, t2.standardDeviation());
+        assertEquals("t2 standardDeviation == 0.0d", 0.0d, t2.standardDeviation());
 
         Timer.time(r0, t0);
         Timer.time(r1, t1);
         Timer.time(r2, t2);
 
-        assertEquals("t0 size == 2", 2, t0.size());
+        assertEquals("t0 size == 2", 2L, t0.size());
         assertTrue("t0 min > 0.0d", t0.min() > 0.0d);
         assertTrue("t0 max > 0.0d", t0.max() > 0.0d);
         assertTrue("t0 sum > 0.0d", t0.sum() > 0.0d);
         assertTrue("t0 mean > 0.0d", t0.mean() > 0.0d);
         assertTrue("t0 standardDeviation > 0.0d", t0.standardDeviation() > 0.0d);
 
-        assertEquals("t1 size == 2", 2, t1.size());
+        assertEquals("t1 size == 2", 2L, t1.size());
         assertTrue("t1 min > 0.0d", t1.min() > 0.0d);
         assertTrue("t1 max > 0.0d", t1.max() > 0.0d);
         assertTrue("t1 sum > 0.0d", t1.sum() > 0.0d);
         assertTrue("t1 mean > 0.0d", t1.mean() > 0.0d);
         assertTrue("t1 standardDeviation > 0.0d", t1.standardDeviation() > 0.0d);
 
-        assertEquals("t2 size == 2", 2, t2.size());
+        assertEquals("t2 size == 2", 2L, t2.size());
         assertTrue("t2 min > 0.0d", t2.min() > 0.0d);
         assertTrue("t2 max > 0.0d", t2.max() > 0.0d);
         assertTrue("t2 sum > 0.0d", t2.sum() > 0.0d);
@@ -364,21 +364,21 @@ public class TimerTest
         assertNotNull(t1);
         assertNotNull(t2);
 
-        assertEquals("t0 size == 500", 500, t0.size());
+        assertEquals("t0 size == 500", 500L, t0.size());
         assertTrue("t0 min > 0.0d", t0.min() > 0.0d);
         assertTrue("t0 max > 0.0d", t0.max() > 0.0d);
         assertTrue("t0 sum > 0.0d", t0.sum() > 0.0d);
         assertTrue("t0 mean > 0.0d", t0.mean() > 0.0d);
         assertTrue("t0 standardDeviation > 0.0d", t0.standardDeviation() > 0.0d);
 
-        assertEquals("t1 size == 50", 50, t1.size());
+        assertEquals("t1 size == 50", 50L, t1.size());
         assertTrue("t1 min > 0.0d", t1.min() > 0.0d);
         assertTrue("t1 max > 0.0d", t1.max() > 0.0d);
         assertTrue("t1 sum > 0.0d", t1.sum() > 0.0d);
         assertTrue("t1 mean > 0.0d", t1.mean() > 0.0d);
         assertTrue("t1 standardDeviation > 0.0d", t1.standardDeviation() > 0.0d);
 
-        assertEquals("t2 size == 5", 5, t2.size());
+        assertEquals("t2 size == 5", 5L, t2.size());
         assertTrue("t2 min > 0.0d", t2.min() > 0.0d);
         assertTrue("t2 max > 0.0d", t2.max() > 0.0d);
         assertTrue("t2 sum > 0.0d", t2.sum() > 0.0d);
@@ -389,9 +389,9 @@ public class TimerTest
         Timer.loop(r1, 50, t1);
         Timer.loop(r2, 5, t2);
 
-        assertEquals("t0 size == 1000", 1000, t0.size());
-        assertEquals("t1 size == 100", 100, t1.size());
-        assertEquals("t2 size == 10", 10, t2.size());
+        assertEquals("t0 size == 1000", 1000L, t0.size());
+        assertEquals("t1 size == 100", 100L, t1.size());
+        assertEquals("t2 size == 10", 10L, t2.size());
     }
 
     public void testLoopOver()
@@ -400,7 +400,7 @@ public class TimerTest
         Map<Runnable,Timer> map0 = Timer.loop(codeBlocks, 10);
 
         assertNotNull("map0 not null", map0);
-        assertEquals("map0 size == 3", 3, map0.size());
+        assertEquals("map0 size == 3", 3L, map0.size());
         assertTrue("map0 contains key r0", map0.containsKey(r0));
         assertTrue("map0 contains key r1", map0.containsKey(r1));
         assertTrue("map0 contains key r2", map0.containsKey(r2));
@@ -412,21 +412,21 @@ public class TimerTest
         Timer t1 = map0.get(r1);
         Timer t2 = map0.get(r2);
 
-        assertEquals("t0 size == 10", 10, t0.size());
+        assertEquals("t0 size == 10", 10L, t0.size());
         assertTrue("t0 min > 0.0d", t0.min() > 0.0d);
         assertTrue("t0 max > 0.0d", t0.max() > 0.0d);
         assertTrue("t0 sum > 0.0d", t0.sum() > 0.0d);
         assertTrue("t0 mean > 0.0d", t0.mean() > 0.0d);
         assertTrue("t0 standardDeviation > 0.0d", t0.standardDeviation() > 0.0d);
 
-        assertEquals("t1 size == 10", 10, t1.size());
+        assertEquals("t1 size == 10", 10L, t1.size());
         assertTrue("t1 min > 0.0d", t1.min() > 0.0d);
         assertTrue("t1 max > 0.0d", t1.max() > 0.0d);
         assertTrue("t1 sum > 0.0d", t1.sum() > 0.0d);
         assertTrue("t1 mean > 0.0d", t1.mean() > 0.0d);
         assertTrue("t1 standardDeviation > 0.0d", t1.standardDeviation() > 0.0d);
 
-        assertEquals("t2 size == 10", 10, t2.size());
+        assertEquals("t2 size == 10", 10L, t2.size());
         assertTrue("t2 min > 0.0d", t2.min() > 0.0d);
         assertTrue("t2 max > 0.0d", t2.max() > 0.0d);
         assertTrue("t2 sum > 0.0d", t2.sum() > 0.0d);
@@ -447,7 +447,7 @@ public class TimerTest
         Map<Runnable,Timer> map1 = Timer.loop(codeBlocks, 2, 5);
 
         assertNotNull("map1 not null", map1);
-        assertEquals("map1 size == 3", 3, map1.size());
+        assertEquals("map1 size == 3", 3L, map1.size());
         assertTrue("map1 contains key r0", map1.containsKey(r0));
         assertTrue("map1 contains key r1", map1.containsKey(r1));
         assertTrue("map1 contains key r2", map1.containsKey(r2));
@@ -459,21 +459,21 @@ public class TimerTest
         Timer t4 = map1.get(r1);
         Timer t5 = map1.get(r2);
 
-        assertEquals("t3 size == 10", 10, t3.size());
+        assertEquals("t3 size == 10", 10L, t3.size());
         assertTrue("t3 min > 0.0d", t3.min() > 0.0d);
         assertTrue("t3 max > 0.0d", t3.max() > 0.0d);
         assertTrue("t3 sum > 0.0d", t3.sum() > 0.0d);
         assertTrue("t3 mean > 0.0d", t3.mean() > 0.0d);
         assertTrue("t3 standardDeviation > 0.0d", t3.standardDeviation() > 0.0d);
 
-        assertEquals("t4 size == 10", 10, t4.size());
+        assertEquals("t4 size == 10", 10L, t4.size());
         assertTrue("t4 min > 0.0d", t4.min() > 0.0d);
         assertTrue("t4 max > 0.0d", t4.max() > 0.0d);
         assertTrue("t4 sum > 0.0d", t4.sum() > 0.0d);
         assertTrue("t4 mean > 0.0d", t4.mean() > 0.0d);
         assertTrue("t4 standardDeviation > 0.0d", t4.standardDeviation() > 0.0d);
 
-        assertEquals("t5 size == 10", 10, t5.size());
+        assertEquals("t5 size == 10", 10L, t5.size());
         assertTrue("t5 min > 0.0d", t5.min() > 0.0d);
         assertTrue("t5 max > 0.0d", t5.max() > 0.0d);
         assertTrue("t5 sum > 0.0d", t5.sum() > 0.0d);
@@ -498,7 +498,7 @@ public class TimerTest
         Map<Runnable,Timer> map0 = Timer.shuffle(codeBlocks0, 10);
 
         assertNotNull("map0 not null", map0);
-        assertEquals("map0 size == 3", 3, map0.size());
+        assertEquals("map0 size == 3", 3L, map0.size());
         assertTrue("map0 contains key r0", map0.containsKey(r0));
         assertTrue("map0 contains key r1", map0.containsKey(r1));
         assertTrue("map0 contains key r2", map0.containsKey(r2));
@@ -510,21 +510,21 @@ public class TimerTest
         Timer t1 = map0.get(r1);
         Timer t2 = map0.get(r2);
 
-        assertEquals("t0 size == 10", 10, t0.size());
+        assertEquals("t0 size == 10", 10L, t0.size());
         assertTrue("t0 min > 0.0d", t0.min() > 0.0d);
         assertTrue("t0 max > 0.0d", t0.max() > 0.0d);
         assertTrue("t0 sum > 0.0d", t0.sum() > 0.0d);
         assertTrue("t0 mean > 0.0d", t0.mean() > 0.0d);
         assertTrue("t0 standardDeviation > 0.0d", t0.standardDeviation() > 0.0d);
 
-        assertEquals("t1 size == 10", 10, t1.size());
+        assertEquals("t1 size == 10", 10L, t1.size());
         assertTrue("t1 min > 0.0d", t1.min() > 0.0d);
         assertTrue("t1 max > 0.0d", t1.max() > 0.0d);
         assertTrue("t1 sum > 0.0d", t1.sum() > 0.0d);
         assertTrue("t1 mean > 0.0d", t1.mean() > 0.0d);
         assertTrue("t1 standardDeviation > 0.0d", t1.standardDeviation() > 0.0d);
 
-        assertEquals("t2 size == 10", 10, t2.size());
+        assertEquals("t2 size == 10", 10L, t2.size());
         assertTrue("t2 min > 0.0d", t2.min() > 0.0d);
         assertTrue("t2 max > 0.0d", t2.max() > 0.0d);
         assertTrue("t2 sum > 0.0d", t2.sum() > 0.0d);
@@ -546,7 +546,7 @@ public class TimerTest
         Map<Runnable,Timer> map1 = Timer.shuffle(codeBlocks1, 2, 5);
 
         assertNotNull("map1 not null", map1);
-        assertEquals("map1 size == 3", 3, map1.size());
+        assertEquals("map1 size == 3", 3L, map1.size());
         assertTrue("map1 contains key r0", map1.containsKey(r0));
         assertTrue("map1 contains key r1", map1.containsKey(r1));
         assertTrue("map1 contains key r2", map1.containsKey(r2));
@@ -558,21 +558,21 @@ public class TimerTest
         Timer t4 = map1.get(r1);
         Timer t5 = map1.get(r2);
 
-        assertEquals("t3 size == 10", 10, t3.size());
+        assertEquals("t3 size == 10", 10L, t3.size());
         assertTrue("t3 min > 0.0d", t3.min() > 0.0d);
         assertTrue("t3 max > 0.0d", t3.max() > 0.0d);
         assertTrue("t3 sum > 0.0d", t3.sum() > 0.0d);
         assertTrue("t3 mean > 0.0d", t3.mean() > 0.0d);
         assertTrue("t3 standardDeviation > 0.0d", t3.standardDeviation() > 0.0d);
 
-        assertEquals("t4 size == 10", 10, t4.size());
+        assertEquals("t4 size == 10", 10L, t4.size());
         assertTrue("t4 min > 0.0d", t4.min() > 0.0d);
         assertTrue("t4 max > 0.0d", t4.max() > 0.0d);
         assertTrue("t4 sum > 0.0d", t4.sum() > 0.0d);
         assertTrue("t4 mean > 0.0d", t4.mean() > 0.0d);
         assertTrue("t4 standardDeviation > 0.0d", t4.standardDeviation() > 0.0d);
 
-        assertEquals("t5 size == 10", 10, t5.size());
+        assertEquals("t5 size == 10", 10L, t5.size());
         assertTrue("t5 min > 0.0d", t5.min() > 0.0d);
         assertTrue("t5 max > 0.0d", t5.max() > 0.0d);
         assertTrue("t5 sum > 0.0d", t5.sum() > 0.0d);
@@ -597,7 +597,7 @@ public class TimerTest
         Map<Runnable,Timer> map0 = Timer.shuffle(codeBlocks0, 10, new Random());
 
         assertNotNull("map0 not null", map0);
-        assertEquals("map0 size == 3", 3, map0.size());
+        assertEquals("map0 size == 3", 3L, map0.size());
         assertTrue("map0 contains key r0", map0.containsKey(r0));
         assertTrue("map0 contains key r1", map0.containsKey(r1));
         assertTrue("map0 contains key r2", map0.containsKey(r2));
@@ -609,21 +609,21 @@ public class TimerTest
         Timer t1 = map0.get(r1);
         Timer t2 = map0.get(r2);
 
-        assertEquals("t0 size == 10", 10, t0.size());
+        assertEquals("t0 size == 10", 10L, t0.size());
         assertTrue("t0 min > 0.0d", t0.min() > 0.0d);
         assertTrue("t0 max > 0.0d", t0.max() > 0.0d);
         assertTrue("t0 sum > 0.0d", t0.sum() > 0.0d);
         assertTrue("t0 mean > 0.0d", t0.mean() > 0.0d);
         assertTrue("t0 standardDeviation > 0.0d", t0.standardDeviation() > 0.0d);
 
-        assertEquals("t1 size == 10", 10, t1.size());
+        assertEquals("t1 size == 10", 10L, t1.size());
         assertTrue("t1 min > 0.0d", t1.min() > 0.0d);
         assertTrue("t1 max > 0.0d", t1.max() > 0.0d);
         assertTrue("t1 sum > 0.0d", t1.sum() > 0.0d);
         assertTrue("t1 mean > 0.0d", t1.mean() > 0.0d);
         assertTrue("t1 standardDeviation > 0.0d", t1.standardDeviation() > 0.0d);
 
-        assertEquals("t2 size == 10", 10, t2.size());
+        assertEquals("t2 size == 10", 10L, t2.size());
         assertTrue("t2 min > 0.0d", t2.min() > 0.0d);
         assertTrue("t2 max > 0.0d", t2.max() > 0.0d);
         assertTrue("t2 sum > 0.0d", t2.sum() > 0.0d);
@@ -645,7 +645,7 @@ public class TimerTest
         Map<Runnable,Timer> map1 = Timer.shuffle(codeBlocks1, 2, 5);
 
         assertNotNull("map1 not null", map1);
-        assertEquals("map1 size == 3", 3, map1.size());
+        assertEquals("map1 size == 3", 3L, map1.size());
         assertTrue("map1 contains key r0", map1.containsKey(r0));
         assertTrue("map1 contains key r1", map1.containsKey(r1));
         assertTrue("map1 contains key r2", map1.containsKey(r2));
@@ -657,21 +657,21 @@ public class TimerTest
         Timer t4 = map1.get(r1);
         Timer t5 = map1.get(r2);
 
-        assertEquals("t3 size == 10", 10, t3.size());
+        assertEquals("t3 size == 10", 10L, t3.size());
         assertTrue("t3 min > 0.0d", t3.min() > 0.0d);
         assertTrue("t3 max > 0.0d", t3.max() > 0.0d);
         assertTrue("t3 sum > 0.0d", t3.sum() > 0.0d);
         assertTrue("t3 mean > 0.0d", t3.mean() > 0.0d);
         assertTrue("t3 standardDeviation > 0.0d", t3.standardDeviation() > 0.0d);
 
-        assertEquals("t4 size == 10", 10, t4.size());
+        assertEquals("t4 size == 10", 10L, t4.size());
         assertTrue("t4 min > 0.0d", t4.min() > 0.0d);
         assertTrue("t4 max > 0.0d", t4.max() > 0.0d);
         assertTrue("t4 sum > 0.0d", t4.sum() > 0.0d);
         assertTrue("t4 mean > 0.0d", t4.mean() > 0.0d);
         assertTrue("t4 standardDeviation > 0.0d", t4.standardDeviation() > 0.0d);
 
-        assertEquals("t5 size == 10", 10, t5.size());
+        assertEquals("t5 size == 10", 10L, t5.size());
         assertTrue("t5 min > 0.0d", t5.min() > 0.0d);
         assertTrue("t5 max > 0.0d", t5.max() > 0.0d);
         assertTrue("t5 sum > 0.0d", t5.sum() > 0.0d);
