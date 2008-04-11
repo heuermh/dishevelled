8567e9c02daceed800e1e7353101f04a35c5ebed
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2008-04-11 17:08:22 +0000

    adding composite fitness and fitness transform

---
+++ trunk/swarm/src/main/java/org/dishevelled/swarm/fitness/FitnessTransform.java
@@ -0,0 +1,76 @@
+/*
+
+    dsh-swarm  Framework for particle swarm optimization algorithms.
+    Copyright (c) 2006-2008 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.swarm.fitness;
+
+import org.dishevelled.swarm.Fitness;
+
+import org.dishevelled.functor.UnaryFunction;
+//import cern.colt...functions.DoubleFunction;
+
+/**
+ * Fitness transform.
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public final class FitnessTransform
+    implements Fitness
+{
+    /** Fitness delegate. */
+    private final Fitness fitness;
+
+    /** Tranform function. */
+    private final UnaryFunction<Double, Double> transform;
+    //private final DoubleFunction transform;
+
+
+    /**
+     * Create a new fitness transform with the specified
+     * fitness function and transform function.
+     *
+     * @param fitness fitness function to transform, must not be null
+     * @param transform transform function, must not be null
+     */
+    public FitnessTransform(final Fitness fitness,
+                            final UnaryFunction<Double, Double> transform)
+    {
+        if (fitness == null)
+        {
+            throw new IllegalArgumentException("fitness must not be null");
+        }
+        if (transform == null)
+        {
+            throw new IllegalArgumentException("transform must not be null");
+        }
+        this.fitness = fitness;
+        this.transform = transform;
+    }
+
+
+    /** {@inheritDoc} */
+    public double score(final double[] position)
+    {
+        return transform.evaluate(fitness.score(position));
+    }
+}
---
+++ trunk/swarm/src/test/java/org/dishevelled/swarm/fitness/FitnessTransformTest.java
@@ -0,0 +1,123 @@
+/*
+
+    dsh-swarm  Framework for particle swarm optimization algorithms.
+    Copyright (c) 2006-2008 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.swarm.fitness;
+
+import java.util.Random;
+
+import org.dishevelled.functor.UnaryFunction;
+
+import org.dishevelled.swarm.Fitness;
+import org.dishevelled.swarm.AbstractFitnessTest;
+
+/**
+ * Unit test for FitnessTransform.
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public final class FitnessTransformTest
+    extends AbstractFitnessTest
+{
+
+    /** {@inheritDoc} */
+    protected Fitness createFitness()
+    {
+        Fitness fitness = new UniformFitness();
+        UnaryFunction<Double, Double> transform = new UnaryFunction<Double, Double>(
+        {
+                /** {@inheritDoc} */
+                public Double evaluate(final Double value)
+                {
+                    return 1.0d;
+                }
+            };
+        return new FitnessTransform(fitness, transform);
+    }
+
+    public void testConstructor()
+    {
+        Fitness fitness = new UniformFitness();
+        UnaryFunction<Double, Double> transform = new UnaryFunction<Double, Double>()
+            {
+                /** {@inheritDoc} */
+                public Double evaluate(final Double value)
+                {
+                    return 1.0d;
+                }
+            };
+
+        Fitness fitnessTransform0 = new FitnessTransform(fitness, transform);
+
+        try
+        {
+            Fitness fitnessTransform = new FitnessTransform(null, transform);
+            fail("ctr(null,) expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e)
+        {
+            // expected
+        }
+        try
+        {
+            Fitness fitnessTransform = new FitnessTransform(fitness, null);
+            fail("ctr(,null) expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e)
+        {
+            // expected
+        }
+        try
+        {
+            Fitness fitnessTransform = new FitnessTransform(null, null);
+            fail("ctr(null,null) expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e)
+        {
+            // expected
+        }
+    }
+
+    public void testFitnessTransform()
+    {
+        Fitness randomFitness = new RandomFitness();
+        UnaryFunction<Double, Double> transform = new UnaryFunction<Double, Double>()
+            {
+                /** {@inheritDoc} */
+                public Double evaluate(final Double value)
+                {
+                    return 1.0d;
+                }
+            };
+
+        Fitness fitnessTransform = new FitnessTransform(randomFitness, transform);
+
+        Random random = new Random();
+        for (int i = 0; i < 100; i++)
+        {
+            double[] position = new double[] { random.nextDouble(), random.nextDouble() };
+            assertTrue(randomFitness.score(position) != fitnessTransform.score(position));
+            assertEquals(1.0d, fitnessTransform.score(position));
+        }
+    }
+}
