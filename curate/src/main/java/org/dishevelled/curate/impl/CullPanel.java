822d4cad074e4aee5c492a7da8c0f52dc88d0c33
heuermh <heuermh@acm.org>
2008-04-29 19:26:30 +0000

    minor style fixes

--- curate/src/main/java/org/dishevelled/curate/impl/CullPanel.java
+++ curate/src/main/java/org/dishevelled/curate/impl/CullPanel.java
@@ -29,9 +29,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -357,7 +355,7 @@ public final class CullPanel<E>
     /**
      * Assist.
      */
-    private class Assist<E>
+    private class Assist
     {
         /** Rate (in ms) at which new selection change events are triggered. */
         private int rate;
