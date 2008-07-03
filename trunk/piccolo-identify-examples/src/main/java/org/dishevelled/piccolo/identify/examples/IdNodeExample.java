02b40e8a41b9dd92e790d271433b77d3cecc7b2e
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2008-07-03 21:57:40 +0000

    adding style labels, simulating a selection model

--- trunk/piccolo-identify-examples/src/main/java/org/dishevelled/piccolo/identify/examples/IdNodeExample.java
+++ trunk/piccolo-identify-examples/src/main/java/org/dishevelled/piccolo/identify/examples/IdNodeExample.java
@@ -26,11 +26,18 @@ package org.dishevelled.piccolo.identify.examples;
 import java.awt.BorderLayout;
 import java.awt.Font;
 
+import java.util.Iterator;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import edu.umd.cs.piccolo.PCanvas;
+import edu.umd.cs.piccolo.PNode;
+
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PDragEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 
 import edu.umd.cs.piccolo.nodes.PText;
 
@@ -43,6 +50,7 @@ import org.dishevelled.iconbundle.tango.TangoProject;
 
 import org.dishevelled.identify.Identifiable;
 
+import org.dishevelled.piccolo.identify.AbstractIdNode;
 import org.dishevelled.piccolo.identify.GenericIdNode;
 import org.dishevelled.piccolo.identify.FinderIdNode;
 import org.dishevelled.piccolo.identify.NautilusIdNode;
@@ -69,97 +77,123 @@ public final class IdNodeExample
     {
         super();
 
-        PCanvas canvas = new PCanvas();
+        final PCanvas canvas = new PCanvas();
         canvas.setAnimatingRenderQuality(PPaintContext.HIGH_QUALITY_RENDERING);
         canvas.setDefaultRenderQuality(PPaintContext.HIGH_QUALITY_RENDERING);
         canvas.setInteractingRenderQuality(PPaintContext.HIGH_QUALITY_RENDERING);
 
+        PText genericLabel = new PText("Generic");
+        genericLabel.offset(0, 50);
+        canvas.getLayer().addChild(genericLabel);
         Font defaultFont = PText.DEFAULT_FONT;
         GenericIdNode idNode0 = new GenericIdNode(new Doc(), TangoProject.EXTRA_SMALL);
         idNode0.setFont(defaultFont.deriveFont(10.0f));
-        idNode0.offset(75, 75);
+        idNode0.offset(75, 0);
         canvas.getLayer().addChild(idNode0);
         GenericIdNode idNode1 = new GenericIdNode(new Mic(), TangoProject.SMALL);
         idNode1.setFont(defaultFont.deriveFont(11.0f));
-        idNode1.offset(150, 150);
+        idNode1.offset(150, 25);
         canvas.getLayer().addChild(idNode1);
         GenericIdNode idNode2 = new GenericIdNode(new Computer(), TangoProject.MEDIUM);
-        idNode2.offset(225, 225);
+        idNode2.offset(225, 50);
         canvas.getLayer().addChild(idNode2);
         GenericIdNode idNode3 = new GenericIdNode(new Doc(), TangoProject.LARGE);
-        idNode3.offset(300, 300);
+        idNode3.offset(300, 75);
         canvas.getLayer().addChild(idNode3);
         GenericIdNode idNode4 = new GenericIdNode(new Mic(), CUSTOM_128X128);
         idNode4.setFont(defaultFont.deriveFont(14.0f));
-        idNode4.offset(375, 375);
+        idNode4.offset(375, 100);
         canvas.getLayer().addChild(idNode4);
 
+        PText finderLabel = new PText("Finder-style");
+        finderLabel.offset(0, 250);
+        canvas.getLayer().addChild(finderLabel);
         FinderIdNode idNode5 = new FinderIdNode(new Doc(), TangoProject.EXTRA_SMALL);
         idNode5.setFont(defaultFont.deriveFont(10.0f));
-        idNode5.offset(75, 475);
+        idNode5.offset(75, 200);
         canvas.getLayer().addChild(idNode5);
         FinderIdNode idNode6 = new FinderIdNode(new Mic(), TangoProject.SMALL);
         idNode6.setFont(defaultFont.deriveFont(11.0f));
-        idNode6.offset(150, 550);
-        idNode6.selected();
+        idNode6.offset(150, 225);
         canvas.getLayer().addChild(idNode6);
         FinderIdNode idNode7 = new FinderIdNode(new Computer(), TangoProject.MEDIUM);
-        idNode7.offset(225, 625);
+        idNode7.offset(225, 250);
         canvas.getLayer().addChild(idNode7);
         FinderIdNode idNode8 = new FinderIdNode(new Doc(), TangoProject.LARGE);
-        idNode8.offset(300, 700);
-        idNode8.selected();
+        idNode8.offset(300, 275);
         canvas.getLayer().addChild(idNode8);
         FinderIdNode idNode9 = new FinderIdNode(new Mic(), CUSTOM_128X128);
         idNode9.setFont(defaultFont.deriveFont(14.0f));
-        idNode9.offset(375, 775);
+        idNode9.offset(375, 300);
         canvas.getLayer().addChild(idNode9);
 
+        PText explorerLabel = new PText("Explorer-style");
+        explorerLabel.offset(0, 450);
+        canvas.getLayer().addChild(explorerLabel);
         Font tahoma = new Font("Tahoma", Font.PLAIN, 11);
         ExplorerIdNode idNode10 = new ExplorerIdNode(new Doc(), TangoProject.EXTRA_SMALL);
         idNode10.setFont(tahoma.deriveFont(10.0f));
-        idNode10.offset(75, 875);
+        idNode10.offset(75, 400);
         canvas.getLayer().addChild(idNode10);
         ExplorerIdNode idNode11 = new ExplorerIdNode(new Mic(), TangoProject.SMALL);
         idNode11.setFont(tahoma);
-        idNode11.offset(150, 950);
-        idNode11.selected();
+        idNode11.offset(150, 425);
         canvas.getLayer().addChild(idNode11);
         ExplorerIdNode idNode12 = new ExplorerIdNode(new Computer(), TangoProject.MEDIUM);
         idNode12.setFont(tahoma);
-        idNode12.offset(225, 1025);
+        idNode12.offset(225, 450);
         canvas.getLayer().addChild(idNode12);
         ExplorerIdNode idNode13 = new ExplorerIdNode(new Doc(), TangoProject.LARGE);
         idNode13.setFont(tahoma);
-        idNode13.offset(300, 1100);
-        idNode13.selected();
+        idNode13.offset(300, 475);
         canvas.getLayer().addChild(idNode13);
         ExplorerIdNode idNode14 = new ExplorerIdNode(new Mic(), CUSTOM_128X128);
         idNode14.setFont(tahoma.deriveFont(13.0f));
-        idNode14.offset(375, 1175);
+        idNode14.offset(375, 500);
         canvas.getLayer().addChild(idNode14);
 
+        PText nautilusLabel = new PText("Nautilus-style");
+        nautilusLabel.offset(0, 650);
+        canvas.getLayer().addChild(nautilusLabel);
         NautilusIdNode idNode15 = new NautilusIdNode(new Doc(), TangoProject.EXTRA_SMALL);
         idNode15.setFont(defaultFont.deriveFont(10.0f));
-        idNode15.offset(75, 1225);
+        idNode15.offset(75, 600);
         canvas.getLayer().addChild(idNode15);
         NautilusIdNode idNode16 = new NautilusIdNode(new Mic(), TangoProject.SMALL);
         idNode16.setFont(defaultFont.deriveFont(11.0f));
-        idNode16.offset(150, 1300);
-        idNode16.selected();
+        idNode16.offset(150, 625);
         canvas.getLayer().addChild(idNode16);
         NautilusIdNode idNode17 = new NautilusIdNode(new Computer(), TangoProject.MEDIUM);
-        idNode17.offset(225, 1375);
-        idNode17.mouseover();
+        idNode17.offset(225, 650);
         canvas.getLayer().addChild(idNode17);
         NautilusIdNode idNode18 = new NautilusIdNode(new Doc(), TangoProject.LARGE);
-        idNode18.offset(300, 1425);
-        idNode18.selected();
+        idNode18.offset(300, 675);
         canvas.getLayer().addChild(idNode18);        
         NautilusIdNode idNode19 = new NautilusIdNode(new Mic(), CUSTOM_128X128);
         idNode19.setFont(defaultFont.deriveFont(14.0f));
-        idNode19.offset(375, 1500);
+        idNode19.offset(375, 700);
         canvas.getLayer().addChild(idNode19);
+
+        // simulate a selection model by deselecting all when clicking outside a pickable node
+        canvas.getCamera().addInputEventListener(new PBasicInputEventHandler()
+            {
+                /** {@inheritDoc} */
+                public void mousePressed(final PInputEvent event)
+                {
+                    if (event.getCamera().equals(event.getPickedNode()))
+                    {
+                        for (Iterator i = canvas.getLayer().getChildrenIterator(); i.hasNext(); )
+                        {
+                            PNode node = (PNode) i.next();
+                            if (node instanceof AbstractIdNode)
+                            {
+                                AbstractIdNode idNode = (AbstractIdNode) node;
+                                idNode.deselect();
+                            }
+                        }
+                    }
+                }
+            });
 
         setLayout(new BorderLayout());
         add("Center", canvas);
