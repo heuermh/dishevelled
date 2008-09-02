f19e5dc1be2cff9e93de40b6d06a2f06da6c3d8c
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2008-09-02 20:59:21 +0000

    adding package-info.java package documentation

---
+++ trunk/functor/src/main/java/org/dishevelled/functor/package-info.java
@@ -0,0 +1,51 @@
+/*
+
+    dsh-functor  Typed functor interfaces.
+    Copyright (c) 2004-2008 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+
+/**
+ * Typed functor object interfaces.
+ * <p>
+ * See also
+ * </p>
+ * <p>
+ * Functor Objects (Wiki)<br/>
+ * &gt; <a href="http://c2.com/cgi/wiki?FunctorObject">http://c2.com/cgi/wiki?FunctorObject</a>
+ * </p>
+ * <p>
+ * Blocks In Java (Wiki)<br/>
+ * &gt; <a href="http://c2.com/cgi/wiki?BlocksInJava">http://c2.com/cgi/wiki?BlocksInJava</a>
+ * </p>
+ * <p>
+ * jga - Generic Algorithms for Java<br/>
+ * &gt; <a href="http://jga.sourceforge.net">http://jga.sourceforge.net</a>
+ * </p>
+ * <p>
+ * Apache Jakarta Commons Functor<br/>
+ * &gt; <a href="http://jakarta.apache.org/commons/sandbox/functor">http://jakarta.apache.org/commons/sandbox/functor</a>
+ * </p>
+ * <p>
+ * Colt Project<br/>
+ * &gt; <a href="http://dsd.lbl.gov/~hoschek/colt">http://dsd.lbl.gov/~hoschek/colt</a><br/>
+ * </p>
+ */
+package org.dishevelled.functor;
