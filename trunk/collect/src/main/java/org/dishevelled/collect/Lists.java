91ee05ce0615fffc3505f1446d3784762fd27194
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2009-01-23 22:58:02 +0000

    updating copyright years

--- trunk/collect/src/main/java/org/dishevelled/collect/Lists.java
+++ trunk/collect/src/main/java/org/dishevelled/collect/Lists.java
@@ -1,7 +1,7 @@
 /*
 
     dsh-collect  Collection and map utility classes.
-    Copyright (c) 2008 held jointly by the individual authors.
+    Copyright (c) 2008-2009 held jointly by the individual authors.
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published
