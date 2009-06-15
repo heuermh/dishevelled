0451f84004b80f8ffb18a783aef9e1932456980e
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2009-06-15 22:28:31 +0000

    adding random floor and random threshold

---
+++ trunk/interpolate/src/test/java/org/dishevelled/interpolate/RandomThresholdTest.java
@@ -0,0 +1,77 @@
+/*
+
+    dsh-interpolate  Interpolation and easing functions.
+    Copyright (c) 2009 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.interpolate;
+
+import java.util.Random;
+
+/**
+ * Unit test for RandomThreshold.
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public final class RandomThresholdTest
+    extends AbstractEasingFunctionTest
+{
+
+    /** {@inheritDoc} */
+    protected AbstractEasingFunction createEasingFunction()
+    {
+        return new RandomThreshold(new Random(), new Linear());
+    }
+
+    public void testConstructor()
+    {
+        Random random = new Random();
+        AbstractEasingFunction function = new Linear();
+        new RandomThreshold(random, function);
+        try
+        {
+            new RandomThreshold(null, function);
+            fail("ctr(null,) expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e)
+        {
+            // expected
+        }
+        try
+        {
+            new RandomThreshold(random, null);
+            fail("ctr(,null) expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e)
+        {
+            // expected
+        }
+        try
+        {
+            new RandomThreshold(null, null);
+            fail("ctr(null, null) expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e)
+        {
+            // expected
+        }
+    }
+}
