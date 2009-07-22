ad86ddba07dab763f835f2ac9eabdfe41f828c31
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2009-07-22 20:23:53 +0000

    adding createFrameList methods

--- trunk/piccolo-sprite/src/main/java/org/dishevelled/piccolo/sprite/Animations.java
+++ trunk/piccolo-sprite/src/main/java/org/dishevelled/piccolo/sprite/Animations.java
@@ -33,6 +33,7 @@ import java.io.IOException;
 import java.net.URL;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -56,7 +57,7 @@ public final class Animations
 
 
     /**
-     * Create and return a new single frame animation from the specified image.
+     * Create and return a new single frame animation from the specified image file.
      *
      * @param image image file
      * @return a new single frame animation from the specified image
@@ -70,7 +71,7 @@ public final class Animations
     }
 
     /**
-     * Create and return a new single frame animation from the specified image.
+     * Create and return a new single frame animation from the specified image URL.
      *
      * @param image image URL
      * @return a new single frame animation from the specified image
@@ -95,6 +96,32 @@ public final class Animations
     }
 
     /**
+     * Create and return an unmodifiable list of images containing all the frame images
+     * specified from <code>baseImage</code>.
+     *
+     * @param baseImage base image file
+     * @param suffix image suffix
+     * @param frames number of frames
+     * @return a new unmodifiable list of images containing all the frame images
+     *    specified from <code>baseImage</code>
+     * @throws IOException if an IO error occurs
+     */
+    public static List<Image> createFrameList(final File baseImage, final String suffix, final int frames)
+        throws IOException
+    {
+        int leadingZeros = (int) (frames / 10) + 1; // is this math correct?
+        String format = "%s%0" + leadingZeros + "d%s";
+        List<Image> images = new ArrayList<Image>(frames);
+        for (int frame = 0; frame < frames; frame++)
+        {
+            File file = new File(String.format(format, new Object[] { baseImage.getPath(), frame, suffix }));
+            Image image = ImageIO.read(file);
+            images.add(image);
+        }
+        return Collections.unmodifiableList(images);
+    }
+
+    /**
      * Create and return a new looped frames animation containing all the frame images
      * specified from <code>baseImage</code>.
      *
@@ -108,16 +135,33 @@ public final class Animations
     public static LoopedFramesAnimation createAnimation(final File baseImage, final String suffix, final int frames)
         throws IOException
     {
+        return new LoopedFramesAnimation(createFrameList(baseImage, suffix, frames));
+    }
+
+    /**
+     * Create and return an unmodifiable list of images containing all the frame images
+     * specified from <code>baseImage</code>.
+     *
+     * @param baseImage base image URL
+     * @param suffix image suffix
+     * @param frames number of frames
+     * @return a new unmodifiable list of images containing all the frame images
+     *    specified from <code>baseImage</code>
+     * @throws IOException if an IO error occurs
+     */
+    public static List<Image> createFrameList(final URL baseImage, final String suffix, final int frames)
+        throws IOException
+    {
         int leadingZeros = (int) (frames / 10) + 1; // is this math correct?
         String format = "%s%0" + leadingZeros + "d%s";
         List<Image> images = new ArrayList<Image>(frames);
         for (int frame = 0; frame < frames; frame++)
         {
-            File file = new File(String.format(format, new Object[] { baseImage.getPath(), frame, suffix }));
-            Image image = ImageIO.read(file);
+            URL url = new URL(String.format(format, new Object[] { baseImage.getPath(), frame, suffix }));
+            Image image = ImageIO.read(url);
             images.add(image);
         }
-        return new LoopedFramesAnimation(images);
+        return Collections.unmodifiableList(images);
     }
 
     /**
@@ -134,16 +178,30 @@ public final class Animations
     public static LoopedFramesAnimation createAnimation(final URL baseImage, final String suffix, final int frames)
         throws IOException
     {
-        int leadingZeros = (int) (frames / 10) + 1; // is this math correct?
-        String format = "%s%0" + leadingZeros + "d%s";
-        List<Image> images = new ArrayList<Image>(frames);
-        for (int frame = 0; frame < frames; frame++)
-        {
-            URL url = new URL(String.format(format, new Object[] { baseImage.getPath(), frame, suffix }));
-            Image image = ImageIO.read(url);
-            images.add(image);
-        }
-        return new LoopedFramesAnimation(images);
+        return new LoopedFramesAnimation(createFrameList(baseImage, suffix, frames));
+    }
+
+    /**
+     * Create and return an unmodifiable list of frame images containing all the frame images
+     * from <code>spriteSheet</code> as specified by the starting location <code>(x, y)</code>
+     * and read horizontally the specified number of frames.
+     *
+     * @param spriteSheet sprite sheet file
+     * @param x starting location x
+     * @param y starting location y
+     * @param width frame width
+     * @param height frame height
+     * @param frames number of frames
+     * @return an unmodifiable list of frame images containing all the frame images
+     *    from <code>spriteSheet</code> as specified by the starting location <code>(x, y)</code>
+     *    and read horizontally the specified number of frames
+     */
+    public static List<Image> createFrameList(final File spriteSheet, final int x, final int y,
+                                              final int width, final int height, final int frames)
+        throws IOException
+    {
+        BufferedImage bufferedImage = ImageIO.read(spriteSheet);
+        return createFrameList(bufferedImage, x, y, width, height, frames);
     }
 
     /**
@@ -166,8 +224,30 @@ public final class Animations
                                                         final int width, final int height, final int frames)
         throws IOException
     {
+        return new LoopedFramesAnimation(createFrameList(spriteSheet, x, y, width, height, frames));
+    }
+
+    /**
+     * Create and return an unmodifiable list of frame images containing all the frame images
+     * from <code>spriteSheet</code> as specified by the starting location <code>(x, y)</code>
+     * and read horizontally the specified number of frames.
+     *
+     * @param spriteSheet sprite sheet file
+     * @param x starting location x
+     * @param y starting location y
+     * @param width frame width
+     * @param height frame height
+     * @param frames number of frames
+     * @return an unmodifiable list of frame images containing all the frame images
+     *    from <code>spriteSheet</code> as specified by the starting location <code>(x, y)</code>
+     *    and read horizontally the specified number of frames
+     */
+    public static List<Image> createFrameList(final URL spriteSheet, final int x, final int y,
+                                              final int width, final int height, final int frames)
+        throws IOException
+    {
         BufferedImage bufferedImage = ImageIO.read(spriteSheet);
-        return createAnimation(bufferedImage, x, y, width, height, frames);
+        return createFrameList(bufferedImage, x, y, width, height, frames);
     }
 
     /**
@@ -190,8 +270,38 @@ public final class Animations
                                                         final int width, final int height, final int frames)
         throws IOException
     {
-        BufferedImage bufferedImage = ImageIO.read(spriteSheet);
-        return createAnimation(bufferedImage, x, y, width, height, frames);
+        return new LoopedFramesAnimation(createFrameList(spriteSheet, x, y, width, height, frames));
+    }
+
+    /**
+     * Create and return an unmodifiable list of frame images containing all the frame images
+     * from <code>spriteSheet</code> as specified by the starting location <code>(x, y)</code>
+     * and read horizontally the specified number of frames.
+     *
+     * @param spriteSheet sprite sheet image
+     * @param x starting location x
+     * @param y starting location y
+     * @param width frame width
+     * @param height frame height
+     * @param frames number of frames
+     * @return an unmodifiable list of frame images containing all the frame images
+     *    from <code>spriteSheet</code> as specified by the starting location <code>(x, y)</code>
+     *    and read horizontally the specified number of frames
+     */
+    public static List<Image> createFrameList(final BufferedImage spriteSheet, final int x, final int y,
+                                              final int width, final int height, final int frames)
+    {
+        if (spriteSheet == null)
+        {
+            throw new IllegalArgumentException("spriteSheet must not be null");
+        }
+        List<Image> images = new ArrayList<Image>(frames);
+        for (int frame = 0; frame < frames; frame++)
+        {
+            Image subimage = spriteSheet.getSubimage(x + (frame * width), y, width, height);
+            images.add(subimage);
+        }
+        return Collections.unmodifiableList(images);
     }
 
     /**
@@ -212,16 +322,17 @@ public final class Animations
     public static LoopedFramesAnimation createAnimation(final BufferedImage spriteSheet, final int x, final int y,
                                                         final int width, final int height, final int frames)
     {
-        if (spriteSheet == null)
-        {
-            throw new IllegalArgumentException("spriteSheet must not be null");
-        }
-        List<Image> images = new ArrayList<Image>(frames);
-        for (int frame = 0; frame < frames; frame++)
-        {
-            Image subimage = spriteSheet.getSubimage(x + (frame * width), y, width, height);
-            images.add(subimage);
-        }
+        return new LoopedFramesAnimation(createFrameList(spriteSheet, x, y, width, height, frames));
+    }
+
+    /**
+     * Create and return a new looped frames animation containing the specified frame images.
+     *
+     * @param images list of frame images, must not be null
+     * @return a new looped frames animation containing the specified frame images
+     */
+    public static LoopedFramesAnimation createAnimation(final List<Image> images)
+    {
         return new LoopedFramesAnimation(images);
     }
 
