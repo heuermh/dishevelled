95d907fcfa8f8f09c1a75d88364082594c25ac81
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2009-12-15 22:37:17 +0000

    adding quaternary venn node and example, in progress

--- trunk/piccolo-venn/src/main/java/org/dishevelled/piccolo/venn/QuaternaryVennNode.java
+++ trunk/piccolo-venn/src/main/java/org/dishevelled/piccolo/venn/QuaternaryVennNode.java
@@ -27,7 +27,10 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Shape;
 
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import java.util.Set;
@@ -40,21 +43,21 @@ import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
 
-import org.dishevelled.venn.TertiaryVennModel;
+import org.dishevelled.venn.QuaternaryVennModel;
 
-import org.dishevelled.venn.model.TertiaryVennModelImpl;
+import org.dishevelled.venn.model.QuaternaryVennModelImpl;
 
 /**
- * Tertiary venn diagram node.
+ * Quaternary venn diagram node.
  *
  * @author  Michael Heuer
  * @version $Revision$ $Date$
  */
-public final class TertiaryVennNode<E>
+public final class QuaternaryVennNode<E>
     extends PNode
 {
-    /** Tertiary venn model. */
-    private TertiaryVennModel<E> model;
+    /** Quaternary venn model. */
+    private QuaternaryVennModel<E> model;
 
     /** Label text for the first list view. */
     private String firstLabelText = DEFAULT_FIRST_LABEL_TEXT;
@@ -64,6 +67,9 @@ public final class TertiaryVennNode<E>
 
     /** Label text for the third list view. */
     private String thirdLabelText = DEFAULT_THIRD_LABEL_TEXT;
+
+    /** Label text for the fourth list view. */
+    private String fourthLabelText = DEFAULT_FOURTH_LABEL_TEXT;
 
     /** Label for the first list view. */
     private final PText firstLabel;
@@ -91,6 +97,15 @@ public final class TertiaryVennNode<E>
 
     /** Label for the third list view's size. */
     private final PText thirdSize;
+
+    /** Label for the fourth list view. */
+    private final PText fourthLabel;
+
+    /** Path node for the fourth list view. */
+    private final PPath fourth;
+
+    /** Label for the fourth list view's size. */
+    private final PText fourthSize;
 
     /** Area node for the intersection. */
     private final AreaNode intersection;
@@ -128,9 +143,12 @@ public final class TertiaryVennNode<E>
     /** Default label text for the third list view, <code>"Third set"</code>. */
     public static final String DEFAULT_THIRD_LABEL_TEXT = "Third set";
 
+    /** Default label text for the third list view, <code>"Fourth set"</code>. */
+    public static final String DEFAULT_FOURTH_LABEL_TEXT = "Fourth set";
+
 
     /**
-     * Create a new tertiary venn node with the specified sets.
+     * Create a new quaternary venn node with the specified sets.
      *
      * @param firstLabelText label text for the first list view
      * @param first first set, must not be null
@@ -138,21 +156,24 @@ public final class TertiaryVennNode<E>
      * @param second second set, must not be null
      * @param thirdLabelText label text for the third list view
      * @param third third set, must not be null
+     * @param fourthLabelText label text for the fourth list view
+     * @param fourth fourth set, must not be null
      */
-    public TertiaryVennNode(final String firstLabelText, final Set<? extends E> first,
-                            final String secondLabelText, final Set<? extends E> second,
-                            final String thirdLabelText, final Set<? extends E> third)
+    public QuaternaryVennNode(final String firstLabelText, final Set<? extends E> first,
+                              final String secondLabelText, final Set<? extends E> second,
+                              final String thirdLabelText, final Set<? extends E> third,
+                              final String fourthLabelText, final Set<? extends E> fourth)
     {
-        this(new TertiaryVennModelImpl<E>(first, second, third));
-        // todo:  not using firstLabelText, secondLabelText, thirdLabelText
+        this(new QuaternaryVennModelImpl<E>(first, second, third, fourth));
+        // todo:  not using firstLabelText, secondLabelText, thirdLabelText, fourthLabelText
     }
 
     /**
-     * Create a new tertiary venn node with the specified model.
+     * Create a new quaternary venn node with the specified model.
      *
-     * @param model model for this tertiary venn node, must not be null
+     * @param model model for this quaternary venn node, must not be null
      */
-    public TertiaryVennNode(final TertiaryVennModel<E> model)
+    public QuaternaryVennNode(final QuaternaryVennModel<E> model)
     {
         super();
         if (model == null)
@@ -161,26 +182,57 @@ public final class TertiaryVennNode<E>
         }
         this.model = model;
 
-        firstLabel = new PText();
         // todo:  match reference
         //   using colors from wikipedia image, choose others or attribute correctly
-        first = PPath.createEllipse(0.0f, 0.0f, 128.0f, 128.0f);
-        first.setPaint(new Color(5, 37, 255, 50));
-        first.setStroke(new BasicStroke(1.0f));
+        // todo:  doesn't appear that translate figures into the bounds calc in layout
+        Ellipse2D ellipse = new Ellipse2D.Double(0.0d, 0.0d, 376.0d, 234.0d);
+        Point2D center = new Point2D.Double(ellipse.getBounds2D().getCenterX(), ellipse.getBounds2D().getCenterY());
+        Area firstArea = new Area(ellipse);
+        firstArea = firstArea.createTransformedArea(AffineTransform.getRotateInstance((-2.0d * Math.PI) / 9.0d, center.getX(), center.getY()));
+        first = new PPath();
+        first.setPathTo(firstArea);
+
+        first.setPaint(new Color(30, 30, 30, 50));
+        first.setStroke(new BasicStroke(0.5f));
         first.setStrokePaint(new Color(20, 20, 20));
+        firstLabel = new PText();
         firstSize = new PText();
-        secondLabel = new PText();
-        second = PPath.createEllipse(((2.0f * 128.0f) / 3.0f), 0.0f, 128.0f, 128.0f);
-        second.setPaint(new Color(255, 100, 5, 50));
-        second.setStroke(new BasicStroke(1.0f));
+
+        Area secondArea = new Area(ellipse);
+        secondArea = secondArea.createTransformedArea(AffineTransform.getRotateInstance((-2.0d * Math.PI) / 9.0d, center.getX(), center.getY()));
+        secondArea = secondArea.createTransformedArea(AffineTransform.getTranslateInstance(105.0d, 85.0d));
+        second = new PPath();
+        second.setPathTo(secondArea);
+
+        second.setPaint(new Color(5, 37, 255, 50));
+        second.setStroke(new BasicStroke(0.5f));
         second.setStrokePaint(new Color(20, 20, 20));
+        secondLabel = new PText();
         secondSize = new PText();
-        thirdLabel = new PText();
-        third = PPath.createEllipse(128.0f / 3.0f, (2.0f * 128.0f) / 3.0f, 128.0f, 128.0f);
-        third.setPaint(new Color(30, 30, 30, 50));
-        third.setStroke(new BasicStroke(1.0f));
+
+        Area thirdArea = new Area(ellipse);
+        thirdArea = thirdArea.createTransformedArea(AffineTransform.getRotateInstance((2.0d * Math.PI) / 9.0d, center.getX(), center.getY()));
+        thirdArea = thirdArea.createTransformedArea(AffineTransform.getTranslateInstance(-105.0d, 85.0d));
+        third = new PPath();
+        third.setPathTo(thirdArea);
+
+        third.setPaint(new Color(255, 100, 5, 50));
+        third.setStroke(new BasicStroke(0.5f));
         third.setStrokePaint(new Color(20, 20, 20));
+        thirdLabel = new PText();
         thirdSize = new PText();
+
+        Area fourthArea = new Area(ellipse);
+        fourthArea = fourthArea.createTransformedArea(AffineTransform.getRotateInstance((2.0d * Math.PI) / 9.0d, center.getX(), center.getY()));
+        fourth = new PPath();
+        fourth.setPathTo(fourthArea);
+
+        fourth.setPaint(new Color(11, 255, 5, 50));
+        fourth.setStroke(new BasicStroke(0.5f));
+        fourth.setStrokePaint(new Color(20, 20, 20));
+        fourthLabel = new PText();
+        fourthSize = new PText();
+
         intersection = new AreaNode();
         intersectionSize = new PText();
         firstSecond = new AreaNode();
@@ -190,6 +242,7 @@ public final class TertiaryVennNode<E>
         secondThird = new AreaNode();
         secondThirdSize = new PText();
 
+        addChild(fourth);
         addChild(third);
         addChild(second);
         addChild(first);
@@ -197,6 +250,7 @@ public final class TertiaryVennNode<E>
         addChild(firstThird);
         addChild(secondThird);
         addChild(intersection);
+        addChild(fourthSize);
         addChild(thirdSize);
         addChild(secondSize);
         addChild(firstSize);
@@ -204,6 +258,7 @@ public final class TertiaryVennNode<E>
         addChild(firstThirdSize);
         addChild(secondThirdSize);
         addChild(intersectionSize);
+        addChild(fourthLabel);
         addChild(thirdLabel);
         addChild(secondLabel);
         addChild(firstLabel);
@@ -255,6 +310,16 @@ public final class TertiaryVennNode<E>
                 }
             });
 
+        model.fourth().addListEventListener(new ListEventListener<E>()
+            {
+                /** {@inheritDoc} */
+                public void listChanged(final ListEvent<E> event)
+                {
+                    fourthLabel.setText(buildLabel(fourthLabelText, model.fourth().size()));
+                    fourthSize.setText(String.valueOf(model.fourth().size() - model.intersection().size()));
+                }
+            });
+
         model.intersection().addListEventListener(new ListEventListener<E>()
             {
                 /** {@inheritDoc} */
@@ -276,6 +341,8 @@ public final class TertiaryVennNode<E>
         secondSize.setText(String.valueOf(model.second().size() - model.intersection().size()));
         thirdLabel.setText(buildLabel(thirdLabelText, model.third().size()));
         thirdSize.setText(String.valueOf(model.third().size() - model.intersection().size()));
+        fourthLabel.setText(buildLabel(fourthLabelText, model.fourth().size()));
+        fourthSize.setText(String.valueOf(model.fourth().size() - model.intersection().size()));
         intersectionSize.setText(String.valueOf(model.intersection().size()));
         firstSecondSize.setText(String.valueOf(model.union(model.first(), model.second()).size() - model.intersection().size()));
         firstThirdSize.setText(String.valueOf(model.union(model.first(), model.third()).size() - model.intersection().size()));
@@ -291,20 +358,29 @@ public final class TertiaryVennNode<E>
         Shape firstShape = first.getPathReference();
         Shape secondShape = second.getPathReference();
         Shape thirdShape = third.getPathReference();
+        Shape fourthShape = fourth.getPathReference();
 
         Area firstArea = new Area(firstShape);
         Area secondArea = new Area(secondShape);
         Area thirdArea = new Area(thirdShape);
+        Area fourthArea = new Area(fourthShape);
         firstArea.subtract(new Area(secondShape));
         firstArea.subtract(new Area(thirdShape));
+        firstArea.subtract(new Area(fourthShape));
         secondArea.subtract(new Area(firstShape));
         secondArea.subtract(new Area(thirdShape));
+        secondArea.subtract(new Area(fourthShape));
         thirdArea.subtract(new Area(firstShape));
         thirdArea.subtract(new Area(secondShape));
+        thirdArea.subtract(new Area(fourthShape));
+        fourthArea.subtract(new Area(firstShape));
+        fourthArea.subtract(new Area(secondShape));
+        fourthArea.subtract(new Area(thirdShape));
 
         Area intersectionArea = new Area(firstShape);
         intersectionArea.intersect(new Area(secondShape));
         intersectionArea.intersect(new Area(thirdShape));
+        intersectionArea.intersect(new Area(fourthShape));
         intersection.setArea(intersectionArea);
 
         Area firstSecondArea = new Area(firstShape);
@@ -325,6 +401,7 @@ public final class TertiaryVennNode<E>
         Rectangle2D firstBounds = firstArea.getBounds2D();
         Rectangle2D secondBounds = secondArea.getBounds2D();
         Rectangle2D thirdBounds = thirdArea.getBounds2D();
+        Rectangle2D fourthBounds = fourthArea.getBounds2D();
         Rectangle2D intersectionBounds = intersectionArea.getBounds2D();
         Rectangle2D firstSecondBounds = firstSecondArea.getBounds2D();
         Rectangle2D firstThirdBounds = firstThirdArea.getBounds2D();
@@ -333,9 +410,11 @@ public final class TertiaryVennNode<E>
         Rectangle2D firstLabelBounds = firstLabel.getFullBoundsReference();
         Rectangle2D secondLabelBounds = secondLabel.getFullBoundsReference();
         Rectangle2D thirdLabelBounds = thirdLabel.getFullBoundsReference();
+        Rectangle2D fourthLabelBounds = fourthLabel.getFullBoundsReference();
         Rectangle2D firstSizeBounds = firstSize.getFullBoundsReference();
         Rectangle2D secondSizeBounds = secondSize.getFullBoundsReference();
         Rectangle2D thirdSizeBounds = thirdSize.getFullBoundsReference();
+        Rectangle2D fourthSizeBounds = fourthSize.getFullBoundsReference();
         Rectangle2D intersectionSizeBounds = intersectionSize.getFullBoundsReference();
         Rectangle2D firstSecondSizeBounds = firstSecondSize.getFullBoundsReference();
         Rectangle2D firstThirdSizeBounds = firstThirdSize.getFullBoundsReference();
@@ -344,15 +423,19 @@ public final class TertiaryVennNode<E>
         firstLabel.setOffset(firstBounds.getX() + (firstBounds.getWidth() / 2.0d) - (firstLabelBounds.getWidth() / 2.0d),
                              -1.0d * firstLabelBounds.getHeight() - 4.0d);
         secondLabel.setOffset(secondBounds.getX() + (secondBounds.getWidth() / 2.0d) - (secondLabelBounds.getWidth() / 2.0d),
-                              -1.0d * secondLabelBounds.getHeight() - 4.0d);
+                              -1.0d * secondLabelBounds.getHeight() - 24.0d);
         thirdLabel.setOffset(thirdBounds.getX() + (thirdBounds.getWidth() / 2.0d) - (thirdLabelBounds.getWidth() / 2.0d),
-                             thirdBounds.getY() + thirdBounds.getHeight() + 6.0d);
+                             -1.0d * thirdLabelBounds.getHeight() - 24.0d);
+        fourthLabel.setOffset(fourthBounds.getX() + (fourthBounds.getWidth() / 2.0d) - (fourthLabelBounds.getWidth() / 2.0d),
+                             -1.0d * fourthLabelBounds.getHeight() - 4.0d);
         firstSize.setOffset(firstBounds.getCenterX() - (firstSizeBounds.getWidth() / 2.0d),
                             firstBounds.getCenterY() - (firstSizeBounds.getHeight() / 2.0d));
         secondSize.setOffset(secondBounds.getCenterX() - (secondSizeBounds.getWidth() / 2.0d),
                             secondBounds.getCenterY() - (secondSizeBounds.getHeight() / 2.0d));
         thirdSize.setOffset(thirdBounds.getCenterX() - (thirdSizeBounds.getWidth() / 2.0d),
                             thirdBounds.getCenterY() - (thirdSizeBounds.getHeight() / 2.0d));
+        fourthSize.setOffset(fourthBounds.getCenterX() - (fourthSizeBounds.getWidth() / 2.0d),
+                             fourthBounds.getCenterY() - (fourthSizeBounds.getHeight() / 2.0d));
 
         // todo:  this label seems to be misplaced +y a bit, maybe the full bounds calc is off
         intersectionSize.setOffset(intersectionBounds.getCenterX() - (intersectionSizeBounds.getWidth() / 2.0d),
@@ -460,6 +543,31 @@ public final class TertiaryVennNode<E>
         this.thirdLabelText = thirdLabelText;
         thirdLabel.setText(buildLabel(this.thirdLabelText, model.third().size()));
         firePropertyChange(-1, "thirdLabelText", this.thirdLabelText, oldThirdLabelText);
+    }
+
+    /**
+     * Return the label text for the fourth list view.  Defaults to {@link #DEFAULT_FOURTH_LABEL_TEXT}.
+     *
+     * @return the label text for the fourth list view
+     */
+    public String getFourthLabelText()
+    {
+        return fourthLabelText;
+    }
+
+    /**
+     * Set the label text for the fourth list view to <code>fourthLabelText</code>.
+     *
+     * <p>This is a bound property.</p>
+     *
+     * @param fourthLabelText label text for the fourth list view
+     */
+    public void setFourthLabelText(final String fourthLabelText)
+    {
+        String oldFourthLabelText = this.fourthLabelText;
+        this.fourthLabelText = fourthLabelText;
+        fourthLabel.setText(buildLabel(this.fourthLabelText, model.fourth().size()));
+        firePropertyChange(-1, "fourthLabelText", this.fourthLabelText, oldFourthLabelText);
     }
 
     /**
