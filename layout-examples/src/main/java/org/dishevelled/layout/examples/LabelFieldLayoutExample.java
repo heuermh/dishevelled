50ecc7823257cdbc68b9b1c2a47da5493b23c2be
heuermh <heuermh@acm.org>
2010-01-03 02:23:15 +0000

    updating copyright years, commit 3 of several

--- layout-examples/src/main/java/org/dishevelled/layout/examples/LabelFieldLayoutExample.java
+++ layout-examples/src/main/java/org/dishevelled/layout/examples/LabelFieldLayoutExample.java
@@ -1,7 +1,7 @@
 /*
 
     dsh-layout-examples  Examples for the layout library.
-    Copyright (c) 2008-2009 held jointly by the individual authors.
+    Copyright (c) 2008-2010 held jointly by the individual authors.
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published
