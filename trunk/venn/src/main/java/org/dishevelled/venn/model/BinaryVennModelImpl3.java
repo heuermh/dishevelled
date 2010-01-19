f527c08a85b907b872d3be0f5e27ae3df5267655
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2010-01-19 23:33:10 +0000

    adding third design of venn model, impl still needs work wrt selection view

---
+++ trunk/venn/src/main/java/org/dishevelled/venn/model/BinaryVennModelImpl3.java
@@ -0,0 +1,145 @@
+/*
+
+    dsh-venn  Lightweight components for venn diagrams.
+    Copyright (c) 2009-2010 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.venn.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+import org.dishevelled.observable.ObservableSet;
+
+import org.dishevelled.observable.impl.ObservableSetImpl;
+
+import org.dishevelled.venn.BinaryVennModel3;
+
+/**
+ * Binary venn model implementation 3.
+ *
+ * @param <E> value type
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public final class BinaryVennModelImpl3<E>
+    implements BinaryVennModel3<E>
+{
+    /** First view. */
+    private final ObservableSet<E> first;
+
+    /** Second view. */
+    private final ObservableSet<E> second;
+
+    /** First only view. */
+    private final Set<E> firstOnly;
+
+    /** Second only view. */
+    private final Set<E> secondOnly;
+
+    /** Intersection view. */
+    private final Set<E> intersection;
+
+    /** Union view. */
+    private final Set<E> union;
+
+    /** Selection view. */
+    private final ObservableSet<E> selection;
+
+
+    /**
+     * Create a new empty binary venn model.
+     */
+    public BinaryVennModelImpl3()
+    {
+        this(new HashSet<E>(), new HashSet<E>());
+    }
+
+    /**
+     * Create a new binary venn model with the specified sets.
+     *
+     * @param first first set, must not be null
+     * @param second second set, must not be null
+     */
+    public BinaryVennModelImpl3(final Set<? extends E> first, final Set<? extends E> second)
+    {
+        if (first == null)
+        {
+            throw new IllegalArgumentException("first must not be null");
+        }
+        if (second == null)
+        {
+            throw new IllegalArgumentException("second must not be null");
+        }
+
+        this.first = new ObservableSetImpl(first);
+        this.second = new ObservableSetImpl(second);
+        firstOnly = Sets.difference(this.first, this.second);
+        secondOnly = Sets.difference(this.second, this.first);
+        intersection = Sets.intersection(this.first, this.second);
+        union = Sets.union(this.first, this.second);
+        selection = new ObservableSetImpl(new HashSet<E>(Math.max(16, union.size())));
+    }
+
+
+    /** {@inheritDoc} */
+    public ObservableSet<E> first()
+    {
+        return first;
+    }
+
+    /** {@inheritDoc} */
+    public ObservableSet<E> second()
+    {
+        return second;
+    }
+
+    /** {@inheritDoc} */
+    public Set<E> firstOnly()
+    {
+        return firstOnly;
+    }
+
+    /** {@inheritDoc} */
+    public Set<E> secondOnly()
+    {
+        return secondOnly;
+    }
+
+    /** {@inheritDoc} */
+    public Set<E> intersection()
+    {
+        return intersection;
+    }
+
+    /** {@inheritDoc} */
+    public Set<E> union()
+    {
+        return union;
+    }
+
+    /** {@inheritDoc} */
+    public ObservableSet<E> selection()
+    {
+        return selection;
+    }
+}
