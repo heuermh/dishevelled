60cad3c4cfabae8e953752506d9476ca64c013bd
heuermh <heuermh@acm.org>
2010-01-20 02:13:01 +0000

    creating selection view to handle responding to mutable view changes

---
+++ venn/src/main/java/org/dishevelled/venn/model/SelectionView.java
@@ -0,0 +1,102 @@
+/*
+
+    dsh-venn  Lightweight components for venn diagrams.
+    Copyright (c) 2009-2010 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.venn.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.dishevelled.observable.ObservableSet;
+
+import org.dishevelled.observable.event.SetChangeEvent;
+import org.dishevelled.observable.event.SetChangeListener;
+
+import org.dishevelled.observable.impl.ObservableSetImpl;
+
+/**
+ * Selection view.
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+final class SelectionView<E>
+    extends ObservableSetImpl<E>
+{
+    /** Union view. */
+    private final Set<E> union;
+
+    /** Purge selection. */
+    private final SetChangeListener<E> purgeSelection = new SetChangeListener()
+        {
+            /** {@inheritDoc} */
+            public void setChanged(final SetChangeEvent event)
+            {
+                purgeSelection();
+            }
+        };
+
+
+    /**
+     * Create a new selection view with the specified views.
+     *
+     * @param union union view
+     * @param views variable number of views
+     */
+    SelectionView(final Set<E> union, final ObservableSet<E>... views)
+    {
+        // todo  null check parameters
+        super(new HashSet<E>(Math.max(16, union == null ? 16 : union.size())));
+        this.union = union;
+        for (ObservableSet<E> view : views)
+        {
+            view.addSetChangeListener(purgeSelection);
+        }
+    }
+
+
+    /**
+     * Remove elements from the selection view not present in the union view.
+     */
+    private void purgeSelection()
+    {
+        Set<E> toRemove = new HashSet<E>(size());
+        for (E e : this)
+        {
+            if (!union.contains(e))
+            {
+                toRemove.add(e);
+            }
+        }
+        removeAll(toRemove);
+    }
+
+    /** {@inheritDoc} */
+    protected boolean preAdd(final E e)
+    {
+        if (!union.contains(e))
+        {
+            throw new IllegalArgumentException("can not select an element not in union");
+        }
+        return super.preAdd(e);
+    }
+}
