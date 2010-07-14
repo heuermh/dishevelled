a8e02b45b8f1cb5a5813ef3e61ffec9e28e7c494
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2010-07-14 22:13:04 +0000

    first checked in

---
+++ trunk/venn-cytoscape-plugin/src/main/java/org/dishevelled/venn/cytoscape/VennCytoscapeAction.java
@@ -0,0 +1,59 @@
+/*
+
+    dsh-venn-cytoscape-plugin  Cytoscape plugin for venn diagrams.
+    Copyright (c) 2010 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.venn.cytoscape;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import cytoscape.Cytoscape;
+
+/**
+ * Action to display the dialog for the venn diagram Cytoscape plugin.
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+final class VennCytoscapeAction
+    extends AbstractAction
+{
+
+    /**
+     * Create a new action to display the dialog for the venn diagram Cytoscape plugin.
+     */
+    VennCytoscapeAction()
+    {
+        super("Venn Diagrams");  // i18n
+    }
+
+
+    /** {@inheritDoc} */
+    public void actionPerformed(final ActionEvent event)
+    {
+        VennCytoscapeDialog dialog = new VennCytoscapeDialog(Cytoscape.getDesktop());
+        // todo:  layout dialog with regards to main frame
+        dialog.setBounds(200, 200, 400, 400);
+        dialog.setVisible(true);
+    }
+}
---
+++ trunk/venn-cytoscape-plugin/src/main/java/org/dishevelled/venn/cytoscape/VennCytoscapeDialog.java
@@ -0,0 +1,115 @@
+/*
+
+    dsh-venn-cytoscape-plugin  Cytoscape plugin for venn diagrams.
+    Copyright (c) 2010 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.venn.cytoscape;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import javax.swing.border.EmptyBorder;
+
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.GlazedLists;
+
+import ca.odell.glazedlists.swing.EventListModel;
+import ca.odell.glazedlists.swing.EventSelectionModel;
+
+import cytoscape.groups.CyGroup;
+import cytoscape.groups.CyGroupManager;
+
+import cytoscape.view.CytoscapeDesktop;
+
+import org.dishevelled.layout.ButtonPanel;
+import org.dishevelled.layout.LabelFieldPanel;
+
+/**
+ * Dialog for the venn diagram Cytoscape plugin.
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+final class VennCytoscapeDialog
+    extends JDialog
+{
+    /** List of groups. */
+    private EventList<CyGroup> groups;
+
+    /** List of groups. */
+    private JList groupList;
+
+
+    /**
+     * Create a new dialog for the venn diagram Cytoscape plugin
+     * with the specified Cytoscape desktop.
+     *
+     * @param desktop Cytoscape desktop
+     */
+    VennCytoscapeDialog(final CytoscapeDesktop desktop)
+    {
+        super(desktop, "Venn Diagrams"); // i18n?
+
+        initComponents();
+        layoutComponents();
+    }
+
+
+    /**
+     * Initialize components.
+     */
+    private void initComponents()
+    {
+        groups = GlazedLists.eventList(CyGroupManager.getGroupList());
+        EventListModel<CyGroup> listModel = new EventListModel<CyGroup>(groups);
+        EventSelectionModel<CyGroup> selectionModel = new EventSelectionModel<CyGroup>(groups);
+        groupList = new JList(listModel);
+        groupList.setSelectionModel(selectionModel);
+    }
+
+    /**
+     * Layout components.
+     */
+    private void layoutComponents()
+    {
+        LabelFieldPanel mainPanel = new LabelFieldPanel();
+        mainPanel.addLabel("Groups:"); // i18n
+        mainPanel.addFinalField(new JScrollPane(groupList));
+
+        ButtonPanel buttonPanel = new ButtonPanel();
+        buttonPanel.setBorder(new EmptyBorder(24, 0, 0, 0));
+        // todo:  actions
+        buttonPanel.add(new JButton("Cancel"));
+        buttonPanel.add(new JButton("OK"));
+
+        JPanel contentPane = new JPanel();
+        contentPane.setLayout(new BorderLayout());
+        contentPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+        contentPane.add("Center", mainPanel);
+        contentPane.add("South", buttonPanel);
+        setContentPane(contentPane);
+    }
+}
