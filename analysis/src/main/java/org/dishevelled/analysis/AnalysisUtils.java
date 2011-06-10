4b17dc1e03210766e176d5a3b195ac148e812147
heuermh <heuermh@acm.org>
2011-06-10 22:17:22 +0000

    first checked in

---
+++ analysis/src/main/java/org/dishevelled/analysis/AnalysisUtils.java
@@ -0,0 +1,167 @@
+/*
+
+    dsh-analysis  Data analysis.
+    Copyright (c) 2011 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.analysis;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.dishevelled.collect.Maps.createMap;
+
+import org.dishevelled.graph.Edge;
+import org.dishevelled.graph.Graph;
+import org.dishevelled.graph.Node;
+
+import static org.dishevelled.graph.impl.GraphUtils.createGraph;
+
+import org.dishevelled.functor.UnaryProcedure;
+import org.dishevelled.functor.TernaryProcedure;
+
+import org.dishevelled.matrix.Matrix2D;
+
+import static org.dishevelled.matrix.impl.SparseMatrixUtils.createSparseMatrix2D;
+
+/**
+ * Static utility methods for data analysis.
+ *
+ * @author  Michael Heuer
+ */
+public final class AnalysisUtils
+{
+
+    /**
+     * Private no-arg constructor.
+     */
+    private AnalysisUtils()
+    {
+        // empty
+    }
+
+
+    /**
+     * Convert the specified graph to a sparse matrix.
+     *
+     * @param <N> graph node type
+     * @param <E> graph edge type and sparse matrix type
+     * @param graph graph, must not be null
+     * @param nodes nodes, must not be null
+     * @return the specified graph converted to a sparse matrix
+     */
+    public static <N, E> Matrix2D<E> toSparseMatrix(final Graph<N, E> graph, final List<Node<N, E>> nodes)
+    {
+        if (graph == null)
+        {
+            throw new IllegalArgumentException("graph must not be null");
+        }
+        if (nodes == null)
+        {
+            throw new IllegalArgumentException("nodes must not be null");
+        }
+        long n = Math.min(graph.nodeCount(), nodes.size());
+        final Matrix2D<E> matrix = createSparseMatrix2D(n, n, graph.edgeCount(), 0.75f);
+        graph.forEachEdge(new UnaryProcedure<Edge<N, E>>()
+                          {
+                              @Override
+                              public void run(final Edge<N, E> edge)
+                              {
+                                  // todo:  provide "merge strategy" for multiple edges
+                                  matrix.set(nodes.indexOf(edge.source()), nodes.indexOf(edge.target()), edge.getValue());
+                              }
+                          });
+        return matrix;
+    }
+
+    public static <E> Graph<Long, E> toGraph(final Matrix2D<E> matrix)
+    {
+        if (matrix == null)
+        {
+            throw new IllegalArgumentException("matrix must not be null");
+        }
+        if (matrix.rows() != matrix.columns())
+        {
+            throw new IllegalArgumentException("matrix must be balanced, rows=" + matrix.rows() + ", columns=" + matrix.columns());
+        }
+        if (matrix.rows() > Integer.MAX_VALUE || matrix.columns() > Integer.MAX_VALUE)
+        {
+            throw new IllegalArgumentException("graph size in number of nodes is limited to " + Integer.MAX_VALUE);
+        }
+        if (matrix.cardinality() > Integer.MAX_VALUE)
+        {
+            throw new IllegalArgumentException("graph size in number of edges is limited to " + Integer.MAX_VALUE);
+        }
+        int n = (int) matrix.rows();
+        int e = (int) matrix.cardinality();
+        // todo:  compare to createLongNonBlockingMap
+        final Map<Long, Node<Long, E>> nodes = createMap(n);
+        final Graph<Long, E> graph = createGraph(n, e);
+        matrix.forEach(new TernaryProcedure<Long, Long, E>()
+                       {
+                           @Override
+                           public void run(final Long row, final Long column, final E value)
+                           {
+                               if (!nodes.containsKey(row))
+                               {
+                                   nodes.put(row, graph.createNode(row));
+                               }
+                               if (!nodes.containsKey(column))
+                               {
+                                   nodes.put(column, graph.createNode(column));
+                               }
+                               if (value != null)
+                               {
+                                   graph.createEdge(nodes.get(row), nodes.get(column), value);
+                               }
+                           }
+                       });
+        return graph;
+    }
+
+    /*
+
+      notes, not necessarily correct
+
+Graph --> Matrix2D and vice versa, or Matrix2D + Map --> Graph
+
+consider moving distance/similarity, interpolation, and similar functions to a separate data analysis project, see LingPipe API for inspiration
+
+extend collect or start something new to pull together matrix, graph, multimap and weightedmap (observable + glazed too?)
+
+To go from Graph to Matrix2D
+
+<N, E> Matrix2D<E> toSparseMatrix(final Graph2D<? extends N, ? extends E> graph);
+<N, E> Matrix2D<E> toSparseMatrix(final Graph2D<? extends N, ? extends E> graph, final List<N> nodes);
+<N, E> Matrix2D<E> toSparseMatrix(final Graph2D<? extends N, ? extends E> graph, final Map<N, Integer> nodes);
+<N, E> Matrix2D<E> toSparseMatrix(final Graph2D<? extends N, ? extends E> graph, final UnaryFunction<N, Integer> nodes);
+
+<N, E> Matrix2D<E> toNonBlockingMatrix(final Graph2D<? extends N, ? extends E> graph);
+
+from Matrix2D to Graph
+
+<N, E> Graph<N, E> toGraph(final Matrix2D<E> matrix);
+<N, E> Graph<N, E> toGraph(final Matrix2D<E> matrix, final List<N> nodes);
+<N, E> Graph<N, E> toGraph(final Matrix2D<E> matrix, final Map<Integer, N> nodes);
+<N, E> Graph<N, E> toGraph(final Matrix2D<E> matrix, final UnaryFunction<Integer, N> nodes);
+
+
+     */
+}
