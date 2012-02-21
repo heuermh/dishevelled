58acc15fc0fbfa94dbabe6624cd520a1f1a70897
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2012-02-21 21:49:08 +0000

    adding venn layout API proposal

---
+++ trunk/piccolo-venn/src/main/java/org/dishevelled/piccolo/venn/BinaryVennLayout.java
@@ -0,0 +1,99 @@
+/*
+
+    dsh-piccolo-venn  Piccolo2D venn diagram nodes and supporting classes.
+    Copyright (c) 2009-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.piccolo.venn;
+
+import java.awt.Shape;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Result of a binary venn diagram layout operation.
+ *
+ * @author  Michael Heuer
+ */
+public final class BinaryVennLayout
+{
+    private final Shape firstShape;
+    private final Shape secondShape;
+    private final Point2D firstOnlyLuneCenter;
+    private final Point2D secondOnlyLuneCenter;
+    private final Point2D intersectionLuneCenter;
+    private final Rectangle2D boundingRectangle;
+    private final double score;
+
+
+    BinaryVennLayout(final Shape firstShape,
+                     final Shape secondShape,
+                     final Point2D firstOnlyLuneCenter,
+                     final Point2D secondOnlyLuneCenter,
+                     final Point2D intersectionLuneCenter,
+                     final Rectangle2D boundingRectangle,
+                     final double score)
+    {
+        this.firstShape = firstShape;
+        this.secondShape = secondShape;
+        this.firstOnlyLuneCenter = firstOnlyLuneCenter;
+        this.secondOnlyLuneCenter = secondOnlyLuneCenter;
+        this.intersectionLuneCenter = intersectionLuneCenter;
+        this.boundingRectangle = boundingRectangle;
+        this.score = score;
+    }
+
+
+    public Shape firstShape()
+    {
+        return firstShape;
+    }
+
+    public Shape secondShape()
+    {
+        return secondShape;
+    }
+
+    public Point2D firstOnlyLuneCenter()
+    {
+        return firstOnlyLuneCenter;
+    }
+
+    public Point2D secondOnlyLuneCenter()
+    {
+        return secondOnlyLuneCenter;
+    }
+
+    public Point2D intersectionLuneCenter()
+    {
+        return intersectionLuneCenter;
+    }
+
+    public Rectangle2D boundingRectangle()
+    {
+        return boundingRectangle;
+    }
+
+    public double score()
+    {
+        return score;
+    }
+}
---
+++ trunk/piccolo-venn/src/main/java/org/dishevelled/piccolo/venn/VennLayout.java
@@ -0,0 +1,67 @@
+/*
+
+    dsh-piccolo-venn  Piccolo2D venn diagram nodes and supporting classes.
+    Copyright (c) 2009-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.piccolo.venn;
+
+import java.awt.geom.Rectangle2D;
+
+import org.dishevelled.venn.BinaryVennModel;
+import org.dishevelled.venn.TernaryVennModel;
+import org.dishevelled.venn.QuaternaryVennModel;
+
+/**
+ * Venn layout.
+ *
+ * @author  Michael Heuer
+ */
+public interface VennLayout
+{
+
+    // let the algorithm choose the best answer
+    BinaryVennLayout layout(BinaryVennModel<?> model, Rectangle2D boundingRectangle, PerformanceHint performanceHint);
+
+    TernaryVennLayout layout(TernaryVennModel<?> model, Rectangle2D boundingRectangle, PerformanceHint performanceHint);
+
+    QuaternaryVennLayout layout(QuaternaryVennModel<?> model, Rectangle2D boundingRectangle, PerformanceHint performanceHint);
+
+
+    // allow the caller to choose the best answer
+
+    /*
+      or Iterable<Future<BinaryVennLayout>> ?
+      or SortedSet<BinaryVennLayout> sorted by score ?
+      or WeightedMap<BinaryVennLayout> ?
+
+    Iterable<BinaryVennLayout> layout(BinaryVennModel<?> model,
+                                      Rectangle2D boundingRectangle,
+                                      Function<BinaryVennLayout, Double> scoringFunction, // return a score for each layout
+                                      Predicate<Double> evaluationPredicate, // return true if a score meets the cutoff
+                                      PerformanceHint performanceHint);
+    */
+
+    enum PerformanceHint
+    {
+        OPTIMIZE_FOR_SPEED,
+        OPTIMIZE_FOR_CORRECTNESS;
+    }
+}
