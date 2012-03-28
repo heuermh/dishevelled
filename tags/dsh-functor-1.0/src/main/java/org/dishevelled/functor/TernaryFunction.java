cb2e07edce8507209beec0c0665b4f196e5b182e
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2012-03-28 20:38:21 +0000

    [maven-release-plugin]  copy for tag dsh-functor-1.0

---
+++ tags/dsh-functor-1.0/src/main/java/org/dishevelled/functor/TernaryFunction.java
@@ -0,0 +1,50 @@
+/*
+
+    dsh-functor  Typed functor interfaces.
+    Copyright (c) 2004-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.functor;
+
+/**
+ * Typed functor that takes three arguments of types <code>E</code>,
+ * <code>F</code>, and <code>G</code> and returns a value of type
+ * <code>RV</code>.
+ *
+ * @param <E> first argument type
+ * @param <F> second argument type
+ * @param <G> third argument type
+ * @param <RV> return value type
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public interface TernaryFunction<E, F, G, RV>
+{
+
+    /**
+     * Evaluate this function with the specified arguments.
+     *
+     * @param e first argument to this evaluation
+     * @param f second argument to this evaluation
+     * @param g third argument to this evaluation
+     * @return the result of this evaluation
+     */
+    RV evaluate(E e, F f, G g);
+}
---
+++ tags/dsh-functor-1.0/src/main/java/org/dishevelled/functor/TernaryPredicate.java
@@ -0,0 +1,48 @@
+/*
+
+    dsh-functor  Typed functor interfaces.
+    Copyright (c) 2004-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.functor;
+
+/**
+ * Typed functor that takes three arguments and returns a <code>boolean</code>
+ * value.
+ *
+ * @param <E> first argument type
+ * @param <F> second argument type
+ * @param <G> third argument type
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public interface TernaryPredicate<E, F, G>
+{
+
+    /**
+     * Evaluate this predicate for the specified arguments.
+     *
+     * @param e first argument to this evaluation
+     * @param f second argument to this evaluation
+     * @param g third argument to this evaluation
+     * @return the result of this evaluation
+     */
+    boolean test(E e, F f, G g);
+}
