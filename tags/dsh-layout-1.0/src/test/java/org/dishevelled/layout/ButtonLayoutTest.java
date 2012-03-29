6e2392470f8e27b08b42a9f41f13d5e849835219
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2012-03-29 01:59:46 +0000

    [maven-release-plugin]  copy for tag dsh-layout-1.0

---
+++ tags/dsh-layout-1.0/src/test/java/org/dishevelled/layout/ButtonLayoutTest.java
@@ -0,0 +1,48 @@
+/*
+
+    dsh-layout  Layout managers for lightweight components.
+    Copyright (c) 2003-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.layout;
+
+import javax.swing.JPanel;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for ButtonLayout.
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public final class ButtonLayoutTest
+    extends TestCase
+{
+
+    public void testConstructor()
+    {
+        ButtonLayout buttonLayout0 = new ButtonLayout(null);
+        assertNotNull(buttonLayout0);
+        JPanel panel = new JPanel();
+        ButtonLayout buttonLayout1 = new ButtonLayout(panel);
+        assertNotNull(buttonLayout1);
+    }
+}
