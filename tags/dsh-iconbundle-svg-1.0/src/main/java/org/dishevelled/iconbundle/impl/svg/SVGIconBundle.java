f8fd5e8378244701dd128f1919015d3747de1f28
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2012-04-05 04:11:49 +0000

    [maven-release-plugin]  copy for tag dsh-iconbundle-svg-1.0

---
+++ tags/dsh-iconbundle-svg-1.0/src/main/java/org/dishevelled/iconbundle/impl/svg/SVGIconBundle.java
@@ -0,0 +1,153 @@
+/*
+
+    dsh-iconbundle-svg  SVG icon bundle implementation.
+    Copyright (c) 2003-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.iconbundle.impl.svg;
+
+import java.io.File;
+
+import java.net.URL;
+
+import java.awt.Image;
+import java.awt.Color;
+import java.awt.Component;
+
+import java.awt.image.BufferedImage;
+
+import javax.swing.UIManager;
+
+import org.dishevelled.iconbundle.IconSize;
+import org.dishevelled.iconbundle.IconState;
+import org.dishevelled.iconbundle.IconBundle;
+import org.dishevelled.iconbundle.IconTextDirection;
+
+import org.dishevelled.iconbundle.impl.IconBundleUtils;
+
+/**
+ * Icon bundle derived from a single scalable base image in SVG format.
+ *
+ * @author  Michael Heuer
+ * @version $Revision: 741 $ $Date: 2010-2012-01-02 20:23:15 -0600 (Sat, 02 Jan 2010-2012) $
+ */
+public final class SVGIconBundle
+    implements IconBundle
+{
+    /** SVG document url. */
+    private URL url;
+
+
+    /**
+     * Create a new SVG icon bundle from a base scalable image that will
+     * be read from the specified file.
+     *
+     * @param file file, must not be null
+     */
+    public SVGIconBundle(final File file)
+    {
+        if (file == null)
+        {
+            throw new IllegalArgumentException("file must not be null");
+        }
+
+        try
+        {
+            url = new URL("file://" + file.getAbsolutePath());
+        }
+        catch (Exception e)
+        {
+            throw new IllegalArgumentException("could not create base image URL: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Create a new SVG icon bundle from a base scalable image that will
+     * the specified URL.
+     *
+     * @param url URL, must not be null
+     */
+    public SVGIconBundle(final URL url)
+    {
+        if (url == null)
+        {
+            throw new IllegalArgumentException("url must not be null");
+        }
+
+        this.url = url;
+    }
+
+
+    /** {@inheritDoc} */
+    public Image getImage(final Component component,
+                          final IconTextDirection direction,
+                          final IconState state,
+                          final IconSize size)
+    {
+        if (direction == null)
+        {
+            throw new IllegalArgumentException("direction must not be null");
+        }
+        if (state == null)
+        {
+            throw new IllegalArgumentException("state must not be null");
+        }
+        if (size == null)
+        {
+            throw new IllegalArgumentException("size must not be null");
+        }
+
+        int height = size.getHeight();
+        int width = size.getWidth();
+
+        BufferedImage image = SVGIconBundleUtils.readSVG(url, width, height);
+
+        if (IconState.ACTIVE == state)
+        {
+            return IconBundleUtils.makeActive(image);
+        }
+        else if (IconState.MOUSEOVER == state)
+        {
+            return IconBundleUtils.makeMouseover(image);
+        }
+        else if (IconState.SELECTED == state)
+        {
+            Color selectionColor = UIManager.getColor("List.selectionBackground");
+            return IconBundleUtils.makeSelected(image, selectionColor);
+        }
+        else if (IconState.SELECTED_MOUSEOVER == state)
+        {
+            Color selectionColor = UIManager.getColor("List.selectionBackground");
+            return IconBundleUtils.makeSelectedMouseover(image, selectionColor);
+        }
+        else if (IconState.DRAGGING == state)
+        {
+            return IconBundleUtils.makeDragging(image);
+        }
+        else if (IconState.DISABLED == state)
+        {
+            return IconBundleUtils.makeDisabled(image);
+        }
+        else
+        {
+            return image;
+        }
+    }
+}
---
+++ tags/dsh-iconbundle-svg-1.0/src/test/java/org/dishevelled/iconbundle/impl/svg/SVGIconBundleTest.java
@@ -0,0 +1,98 @@
+/*
+
+    dsh-iconbundle-svg  SVG icon bundle implementation.
+    Copyright (c) 2003-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.iconbundle.impl.svg;
+
+import java.io.File;
+
+import java.net.URL;
+
+import org.apache.commons.lang.SystemUtils;
+
+import org.dishevelled.iconbundle.IconBundle;
+import org.dishevelled.iconbundle.AbstractIconBundleTest;
+
+/**
+ * Unit test for SVGIconBundle.
+ *
+ * @author  Michael Heuer
+ * @version $Revision: 741 $ $Date: 2010-2012-01-02 20:23:15 -0600 (Sat, 02 Jan 2010-2012) $
+ */
+public final class SVGIconBundleTest
+    extends AbstractIconBundleTest
+{
+    /** File url prefix. */
+    private static final String FILE_URL_PREFIX = SystemUtils.IS_OS_WINDOWS ? "file:///" : "file://";
+
+    /** Relative path to test image. */
+    private static final String TEST_IMAGE_PATH = "./src/test/resources/org/dishevelled/iconbundle/impl/svg/test.svg";
+
+
+    /** @see AbstractIconBundleTest */
+    public IconBundle createIconBundle()
+    {
+        File file = null;
+        URL url = null;
+        try
+        {
+            file = new File(TEST_IMAGE_PATH);
+            url = new URL(FILE_URL_PREFIX + file.getAbsolutePath());
+        }
+        catch (Exception e)
+        {
+            // ignore
+        }
+
+        return new SVGIconBundle(url);
+    }
+
+    public void testConstructor()
+        throws Exception
+    {
+        File file = new File(TEST_IMAGE_PATH);
+        IconBundle ib1 = new SVGIconBundle(file);
+
+        URL url = new URL(FILE_URL_PREFIX + file.getAbsolutePath());
+        IconBundle ib2 = new SVGIconBundle(url);
+
+        try
+        {
+            IconBundle ib = new SVGIconBundle((File) null);
+            fail("ctr((File) null) expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e)
+        {
+            // expected
+        }
+
+        try
+        {
+            IconBundle ib = new SVGIconBundle((URL) null);
+            fail("ctr((URL) null) expected IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e)
+        {
+            // expected
+        }
+    }
+}
