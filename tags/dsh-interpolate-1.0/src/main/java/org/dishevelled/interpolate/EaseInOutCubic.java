031f85085ffae8406c48d7fc9004f585b70b3def
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2012-04-05 03:34:34 +0000

    [maven-release-plugin]  copy for tag dsh-interpolate-1.0

---
+++ tags/dsh-interpolate-1.0/src/main/java/org/dishevelled/interpolate/EaseInOutCubic.java
@@ -0,0 +1,54 @@
+/*
+
+    dsh-interpolate  Interpolation and easing functions.
+    Copyright (c) 2009-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.interpolate;
+
+/**
+ * Ease-in-out cubic interpolation function.
+ * <p><img src="../../../../images/ease-in-out-cubic.png" alt="ease-in-out cubic graph" /></p>
+ *
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public final class EaseInOutCubic
+    implements EasingFunction
+{
+
+    /** {@inheritDoc} */
+    public String toString()
+    {
+        return "ease-in-out-cubic";
+    }
+
+    /** {@inheritDoc} */
+    public Double evaluate(final Double value)
+    {
+        double v = 2.0d * value;
+        if (v < 1.0d)
+        {
+            return 0.5d * Math.pow(v, 3.0d);
+        }
+        v -= 2.0d;
+        return 0.5d * (Math.pow(v, 3.0d) + 2.0d);
+    }
+}
---
+++ tags/dsh-interpolate-1.0/src/main/java/org/dishevelled/interpolate/RandomFloor.java
@@ -0,0 +1,64 @@
+/*
+
+    dsh-interpolate  Interpolation and easing functions.
+    Copyright (c) 2009-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.interpolate;
+
+import java.util.Random;
+
+/**
+ * Random floor interpolation function.  Generates random values (in red)
+ * between the value and <code>1.0d</code>.  May be composed with an easing function (in blue).
+ * <p><img src="../../../../images/random-floor.png" alt="random floor graph" /></p>
+ *
+ * @see EasingFunctions#compose
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+public final class RandomFloor
+    implements EasingFunction
+{
+    /** Source of randomness. */
+    private final Random random;
+
+
+    /**
+     * Create a new random floor interpolation function with the specified source of randomness.
+     *
+     * @param random source of randomness, must not be null
+     */
+    public RandomFloor(final Random random)
+    {
+        if (random == null)
+        {
+            throw new IllegalArgumentException("random must not be null");
+        }
+        this.random = random;
+    }
+
+
+    /** {@inheritDoc} */
+    public Double evaluate(final Double value)
+    {
+        return Math.min(1.0d, value + random.nextDouble() * (1.0d - value));
+    }
+}
