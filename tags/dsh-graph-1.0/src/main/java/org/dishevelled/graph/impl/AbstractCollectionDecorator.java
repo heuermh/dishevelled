9de2a113396a59f53b04f94dbf90412db081252b
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2012-04-06 03:31:41 +0000

    [maven-release-plugin]  copy for tag dsh-graph-1.0

---
+++ tags/dsh-graph-1.0/src/main/java/org/dishevelled/graph/impl/AbstractCollectionDecorator.java
@@ -0,0 +1,158 @@
+/*
+
+    dsh-graph  Directed graph interface and implementation.
+    Copyright (c) 2004-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.graph.impl;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Abstract collection that decorates an instance of <code>Collection</code>.
+ *
+ * @param <E> collection element type
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+abstract class AbstractCollectionDecorator<E>
+    implements Collection<E>
+{
+    /** Collection this decorator decorates. */
+    private final Collection<E> collection;
+
+
+    /**
+     * Create a new abstract collection that
+     * decorates the specified collection.
+     *
+     * @param collection collection to decorate, must not be null
+     */
+    protected AbstractCollectionDecorator(final Collection<E> collection)
+    {
+        if (collection == null)
+        {
+            throw new IllegalArgumentException("collection must not be null");
+        }
+        this.collection = collection;
+    }
+
+
+    /**
+     * Return a reference to the collection this decorator decorates.
+     *
+     * @return a reference to the collection this decorator decorates
+     */
+    protected final Collection<E> getCollection()
+    {
+        return collection;
+    }
+
+    /** {@inheritDoc} */
+    public boolean add(final E e)
+    {
+        return collection.add(e);
+    }
+
+    /** {@inheritDoc} */
+    public boolean addAll(final Collection<? extends E> c)
+    {
+        return collection.addAll(c);
+    }
+
+    /** {@inheritDoc} */
+    public void clear()
+    {
+        collection.clear();
+    }
+
+    /** {@inheritDoc} */
+    public boolean contains(final Object o)
+    {
+        return collection.contains(o);
+    }
+
+    /** {@inheritDoc} */
+    public boolean containsAll(final Collection<?> c)
+    {
+        return collection.containsAll(c);
+    }
+
+    /** {@inheritDoc} */
+    public boolean equals(final Object o)
+    {
+        return collection.equals(o);
+    }
+
+    /** {@inheritDoc} */
+    public int hashCode()
+    {
+        return collection.hashCode();
+    }
+
+    /** {@inheritDoc} */
+    public boolean isEmpty()
+    {
+        return collection.isEmpty();
+    }
+
+    /** {@inheritDoc} */
+    public Iterator<E> iterator()
+    {
+        return collection.iterator();
+    }
+
+    /** {@inheritDoc} */
+    public boolean remove(final Object o)
+    {
+        return collection.remove(o);
+    }
+
+    /** {@inheritDoc} */
+    public boolean removeAll(final Collection<?> c)
+    {
+        return collection.removeAll(c);
+    }
+
+    /** {@inheritDoc} */
+    public boolean retainAll(final Collection<?> c)
+    {
+        return collection.retainAll(c);
+    }
+
+    /** {@inheritDoc} */
+    public int size()
+    {
+        return collection.size();
+    }
+
+    /** {@inheritDoc} */
+    public Object[] toArray()
+    {
+        return collection.toArray();
+    }
+
+    /** {@inheritDoc} */
+    public <T> T[] toArray(final T[] a)
+    {
+        return collection.toArray(a);
+    }
+}
---
+++ tags/dsh-graph-1.0/src/main/java/org/dishevelled/graph/impl/AbstractIteratorDecorator.java
@@ -0,0 +1,84 @@
+/*
+
+    dsh-graph  Directed graph interface and implementation.
+    Copyright (c) 2004-2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.graph.impl;
+
+import java.util.Iterator;
+
+/**
+ * Abstract iterator that decorates an instance of <code>Iterator</code>.
+ *
+ * @param <E> iterator element type
+ * @author  Michael Heuer
+ * @version $Revision$ $Date$
+ */
+abstract class AbstractIteratorDecorator<E>
+    implements Iterator<E>
+{
+    /** Iterator this decorator decorates. */
+    private final Iterator<E> iterator;
+
+
+    /**
+     * Create a new abstract iterator that decorates the specified iterator.
+     *
+     * @param iterator iterator to decorate, must not be null
+     */
+    protected AbstractIteratorDecorator(final Iterator<E> iterator)
+    {
+        if (iterator == null)
+        {
+            throw new IllegalArgumentException("iterator must not be null");
+        }
+        this.iterator = iterator;
+    }
+
+
+    /**
+\     * Return a reference to the iterator this decorator decorates.
+     *
+     * @return a reference to the iterator this decorator decorates
+     */
+    protected final Iterator<E> getIterator()
+    {
+        return iterator;
+    }
+
+    /** {@inheritDoc} */
+    public boolean hasNext()
+    {
+        return iterator.hasNext();
+    }
+
+    /** {@inheritDoc} */
+    public E next()
+    {
+        return iterator.next();
+    }
+
+    /** {@inheritDoc} */
+    public void remove()
+    {
+        iterator.remove();
+    }
+}
