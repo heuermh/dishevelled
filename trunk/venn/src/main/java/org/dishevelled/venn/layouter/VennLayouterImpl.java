ea7085ca4b97a94283d2a0267c38ec9308e88bcf
heuermh <heuermh@b4402f04-0a19-0410-951a-96ca7ad59327>
2012-05-18 22:18:53 +0000

    refactoring VennLayouterImpl, adding ternary layouter algorithm

--- trunk/venn/src/main/java/org/dishevelled/venn/layouter/VennLayouterImpl.java
+++ trunk/venn/src/main/java/org/dishevelled/venn/layouter/VennLayouterImpl.java
@@ -23,22 +23,24 @@
 */
 package org.dishevelled.venn.layouter;
 
-//import java.awt.geom.Ellipse2D;
-//import java.awt.geom.Point2D;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
-//import java.util.Iterator;
-
 import org.dishevelled.venn.VennModel;
-//import org.dishevelled.venn.BinaryVennModel;
-//import org.dishevelled.venn.TernaryVennModel;
-//import org.dishevelled.venn.QuaternaryVennModel;
-
-//import org.dishevelled.venn.BinaryVennLayout;
-//import org.dishevelled.venn.TernaryVennLayout;
-//import org.dishevelled.venn.QuaternaryVennLayout;
+import org.dishevelled.venn.BinaryVennLayout;
+import org.dishevelled.venn.BinaryVennModel;
+import org.dishevelled.venn.TernaryVennLayout;
+import org.dishevelled.venn.TernaryVennModel;
+import org.dishevelled.venn.QuaternaryVennLayout;
+import org.dishevelled.venn.QuaternaryVennModel;
 import org.dishevelled.venn.VennLayout;
 import org.dishevelled.venn.VennLayouter;
+
+import org.dishevelled.venn.layout.BinaryVennLayoutImpl;
+import org.dishevelled.venn.layout.TernaryVennLayoutImpl;
+import org.dishevelled.venn.layout.QuaternaryVennLayoutImpl;
 
 /**
  * Venn layout algorithm.
@@ -66,58 +68,34 @@ public final class VennLayouterImpl implements VennLayouter
             throw new IllegalArgumentException("performanceHint must not be null");
         }
 
-        return null;
-    }
+        if (model instanceof BinaryVennModel)
+        {
+            return layout((BinaryVennModel<?>) model, boundingRectangle);
+        }
+        else if (model instanceof TernaryVennModel)
+        {
+            return layout((TernaryVennModel<?>) model, boundingRectangle);
+        }
+        else if (model instanceof QuaternaryVennModel)
+        {
+            return layout((QuaternaryVennModel<?>) model, boundingRectangle);
+        }
 
-    /** {@inheritDoc} */
-    /*
-    public Iterator<VennLayout> layout(final VennModel<?> model,
-                                       final Rectangle2D boundingRectangle,
-                                       final ScoringFunction scoringFunction,
-                                       final ScorePredicate scorePredicate,
-                                       final PerformanceHint performanceHint)
-    {
-        if (model == null)
-        {
-            throw new IllegalArgumentException("model must not be null");
-        }
-        if (boundingRectangle == null)
-        {
-            throw new IllegalArgumentException("boundingRectangle must not be null");
-        }
-        if (scoringFunction == null)
-        {
-            throw new IllegalArgumentException("scoringFunction must not be null");
-        }
-        if (scorePredicate == null)
-        {
-            throw new IllegalArgumentException("scorePredicate must not be null");
-        }
-        if (performanceHint == null)
-        {
-            throw new IllegalArgumentException("performanceHint must not be null");
-        }
+        // todo: apply venn/euler algorithm
 
         return null;
     }
 
-    public BinaryVennLayout layout(final BinaryVennModel<?> model,
-                                   final Rectangle2D boundingRectangle,
-                                   final PerformanceHint performanceHint)
+    /**
+     * Layout the specified binary venn diagram within the specified bounding rectangle.
+     *
+     * @param model binary venn model, must not be null
+     * @param boundingRectangle bounding rectangle, must not be null
+     * @return the result of the layout operation
+     */
+    BinaryVennLayout layout(final BinaryVennModel<?> model,
+                            final Rectangle2D boundingRectangle)
     {
-        if (model == null)
-        {
-            throw new IllegalArgumentException("model must not be null");
-        }
-        if (boundingRectangle == null)
-        {
-            throw new IllegalArgumentException("boundingRectangle must not be null");
-        }
-        if (performanceHint == null)
-        {
-            throw new IllegalArgumentException("performanceHint must not be null");
-        }
-
         double w = boundingRectangle.getWidth();
         double h = boundingRectangle.getHeight();
         double cx = w / 2.0d;
@@ -137,42 +115,101 @@ public final class VennLayouterImpl implements VennLayouter
                                         firstOnlyCenter, secondOnlyCenter, intersectionCenter, boundingRectangle);
     }
 
-    public TernaryVennLayout layout(final TernaryVennModel<?> model,
-                                    final Rectangle2D boundingRectangle,
-                                    final PerformanceHint performanceHint)
+    /**
+     * Layout the specified ternary venn diagram within the specified bounding rectangle.
+     *
+     * @param model ternary venn model, must not be null
+     * @param boundingRectangle bounding rectangle, must not be null
+     * @return the result of the layout operation
+     */
+    TernaryVennLayout layout(final TernaryVennModel<?> model,
+                             final Rectangle2D boundingRectangle)
     {
-        if (model == null)
-        {
-            throw new IllegalArgumentException("model must not be null");
-        }
-        if (boundingRectangle == null)
-        {
-            throw new IllegalArgumentException("boundingRectangle must not be null");
-        }
-        if (performanceHint == null)
-        {
-            throw new IllegalArgumentException("performanceHint must not be null");
-        }
+        double w = boundingRectangle.getWidth();
+        double h = boundingRectangle.getHeight();
+        double d = 5.0d / (3.0d * Math.min(w, h));
+
+        Ellipse2D firstShape = new Ellipse2D.Double(0.0d, 0.0d, d, d);
+        Ellipse2D secondShape = new Ellipse2D.Double(((2.0d * d) / 3.0d), 0.0d, d, d);
+        Ellipse2D thirdShape = new Ellipse2D.Double(d / 3.0d, (2.0d * d) / 3.0d, d, d);
+
+        Area f = new Area(firstShape);
+        Area s = new Area(secondShape);
+        Area t = new Area(thirdShape);
+
+        Area firstOnly = new Area();
+        firstOnly.add(f);
+        firstOnly.subtract(s);
+        firstOnly.subtract(t);
+        Point2D firstOnlyCenter = centerOf(firstOnly);
+
+        Area secondOnly = new Area();
+        secondOnly.add(s);
+        secondOnly.subtract(f);
+        secondOnly.subtract(t);
+        Point2D secondOnlyCenter = centerOf(secondOnly);
+
+        Area thirdOnly = new Area();
+        thirdOnly.add(t);
+        thirdOnly.subtract(f);
+        thirdOnly.subtract(s);
+        Point2D thirdOnlyCenter = centerOf(thirdOnly);
+
+        Area firstSecond = new Area();
+        firstSecond.add(f);
+        firstSecond.intersect(s);
+        firstSecond.subtract(t);
+        Point2D firstSecondCenter = centerOf(firstSecond);
+
+        Area firstThird = new Area();
+        firstThird.add(f);
+        firstThird.intersect(t);
+        firstThird.subtract(s);
+        Point2D firstThirdCenter = centerOf(firstThird);
+
+        Area secondThird = new Area();
+        secondThird.add(s);
+        secondThird.intersect(t);
+        secondThird.subtract(f);
+        Point2D secondThirdCenter = centerOf(secondThird);
+
+        Area intersection = new Area();
+        intersection.add(f);
+        intersection.intersect(s);
+        intersection.intersect(t);
+        Point2D intersectionCenter = centerOf(intersection);
+
+        return new TernaryVennLayoutImpl(firstShape, secondShape, thirdShape, firstOnlyCenter, secondOnlyCenter,
+                                         thirdOnlyCenter, firstSecondCenter, firstThirdCenter, secondThirdCenter,
+                                         intersectionCenter, boundingRectangle);
+    }
+
+    /**
+     * Layout the specified quaternary venn diagram within the specified bounding rectangle.
+     *
+     * @param model quaternary venn model, must not be null
+     * @param boundingRectangle bounding rectangle, must not be null
+     * @return the result of the layout operation
+     */
+    QuaternaryVennLayout layout(final QuaternaryVennModel<?> model,
+                                final Rectangle2D boundingRectangle)
+    {
         return null;
     }
 
-    public QuaternaryVennLayout layout(final QuaternaryVennModel<?> model,
-                                       final Rectangle2D boundingRectangle,
-                                       final PerformanceHint performanceHint)
+    /**
+     * Find and return the center of the specified area using its bounds rectangle.
+     *
+     * @param area area, must not be null
+     * @return the center of the specified area using its bounds rectangle
+     */
+    static Point2D centerOf(final Area area)
     {
-        if (model == null)
+        if (area == null)
         {
-            throw new IllegalArgumentException("model must not be null");
+            throw new IllegalArgumentException("area must not be null");
         }
-        if (boundingRectangle == null)
-        {
-            throw new IllegalArgumentException("boundingRectangle must not be null");
-        }
-        if (performanceHint == null)
-        {
-            throw new IllegalArgumentException("performanceHint must not be null");
-        }
-        return null;
+        Rectangle2D rectangle = area.getBounds2D();
+        return new Point2D.Double(rectangle.getCenterX(), rectangle.getCenterY());
     }
-    */
 }
