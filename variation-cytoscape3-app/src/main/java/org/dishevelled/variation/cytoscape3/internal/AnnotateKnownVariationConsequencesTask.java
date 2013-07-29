21b445c59ecd2b16076a5be667f8abc8338386ff
heuermh <heuermh@acm.org>
2013-07-29 22:58:22 +0000

    adding annotate known variation consequences action, temporary dialog with tool bar

--- variation-cytoscape3-app/src/main/java/org/dishevelled/variation/cytoscape3/internal/AnnotateKnownVariationConsequencesTask.java
+++ variation-cytoscape3-app/src/main/java/org/dishevelled/variation/cytoscape3/internal/AnnotateKnownVariationConsequencesTask.java
@@ -23,9 +23,18 @@
 */
 package org.dishevelled.variation.cytoscape3.internal;
 
+import static org.dishevelled.variation.cytoscape3.internal.SequenceOntology.countAssignments;
+import static org.dishevelled.variation.cytoscape3.internal.SequenceOntology.indexByName;
+import static org.dishevelled.variation.cytoscape3.internal.SequenceOntology.sequenceVariants;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableSet;
 
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNode;
@@ -39,13 +48,24 @@ import org.dishevelled.variation.Feature;
 import org.dishevelled.variation.FeatureService;
 import org.dishevelled.variation.Variation;
 import org.dishevelled.variation.VariationService;
+import org.dishevelled.variation.VariationConsequence;
+import org.dishevelled.variation.VariationConsequenceService;
+
+import org.dishevelled.vocabulary.AbstractAssignable;
+import org.dishevelled.vocabulary.Assignable;
+import org.dishevelled.vocabulary.Authority;
+import org.dishevelled.vocabulary.Assignment;
+import org.dishevelled.vocabulary.Concept;
+import org.dishevelled.vocabulary.Domain;
+import org.dishevelled.vocabulary.Evidence;
+import org.dishevelled.vocabulary.Relation;
 
 /**
- * Annotate known variations task.
+ * Annotate known variation consequences task.
  *
  * @author  Michael Heuer
  */
-final class AnnotateKnownVariationsTask
+final class AnnotateKnownVariationConsequencesTask
     extends AbstractTask
 {
     /** Species. */
@@ -66,22 +86,27 @@ final class AnnotateKnownVariationsTask
     /** Variation service. */
     private final VariationService variationService;
 
+    /** Variation consequence service. */
+    private final VariationConsequenceService variationConsequenceService;
+
 
     /**
-     * Create a new annotate known variations task.
+     * Create a new annotate known variation consequences task.
      *
      * @param species species, must not be null
      * @param reference reference, must not be null
      * @param ensemblGeneIdColumn ensembl gene id column, must not be null
      * @param featureService feature service, must not be null
      * @param variationService variation service, must not be null
+     * @param variationConsequenceService variation consequence service, must not be null
      */
-    AnnotateKnownVariationsTask(final String species,
-                                final String reference,
-                                final String ensemblGeneIdColumn,
-                                final CyNetwork network,
-                                final FeatureService featureService,
-                                final VariationService variationService)
+    AnnotateKnownVariationConsequencesTask(final String species,
+                                           final String reference,
+                                           final String ensemblGeneIdColumn,
+                                           final CyNetwork network,
+                                           final FeatureService featureService,
+                                           final VariationService variationService,
+                                           final VariationConsequenceService variationConsequenceService)
     {
         checkNotNull(species);
         checkNotNull(reference);
@@ -89,6 +114,7 @@ final class AnnotateKnownVariationsTask
         checkNotNull(network);
         checkNotNull(featureService);
         checkNotNull(variationService);
+        checkNotNull(variationConsequenceService);
 
         this.species = species;
         this.reference = reference;
@@ -96,13 +122,14 @@ final class AnnotateKnownVariationsTask
         this.network = network;
         this.featureService = featureService;
         this.variationService = variationService;
+        this.variationConsequenceService = variationConsequenceService;
     }
 
 
     @Override
     public void run(final TaskMonitor taskMonitor)
     {
-        taskMonitor.setTitle("Annotate known variations");
+        taskMonitor.setTitle("Annotate known variation consequences");
         taskMonitor.setProgress(0.0d);
 
         List<CyNode> nodes = network.getNodeList();
@@ -118,8 +145,19 @@ final class AnnotateKnownVariationsTask
                 {
                     taskMonitor.setStatusMessage("Retrieving variations associated with Ensembl Gene " + ensemblGeneId + " in the region " + feature.getName() + ":" + feature.getStart() + "-" + feature.getEnd() + ":" + feature.getStrand() + "...");
                     List<Variation> variations = variationService.variations(feature);
-                    addCount(node, network, variations.size());
                     taskMonitor.setStatusMessage("Found " + variations.size() + " variations associated with Ensembl Gene " + ensemblGeneId);
+
+                    List<VariationConsequence> allVariationConsequences = new ArrayList<VariationConsequence>(variations.size());
+                    for (Variation variation : variations)
+                    {
+                        taskMonitor.setStatusMessage("Retrieving variation consequences associated with variation " + variation.getIdentifier() + "...");
+                        List<VariationConsequence> variationConsequences = variationConsequenceService.consequences(variation);
+                        allVariationConsequences.addAll(variationConsequences);
+                        taskMonitor.setStatusMessage("Found " + variationConsequences.size() + " variation consequences associated with variation " + variation.getIdentifier());
+                        slowDown();
+                    }
+                    addCount(node, network, allVariationConsequences.size());
+                    addConsequenceCounts(node, network, allVariationConsequences);
                     slowDown();
                 }
                 slowDown();
@@ -140,33 +178,29 @@ final class AnnotateKnownVariationsTask
     {
         CyTable table = network.getDefaultNodeTable();
         CyRow row = table.getRow(node.getSUID());
-        if (table.getColumn("variation_count") == null)
+        if (table.getColumn("variation_consequence_count") == null)
         {
-            table.createColumn("variation_count", Integer.class, false);
+            table.createColumn("variation_consequence_count", Integer.class, false);
         }
-        row.set("variation_count", count);
+        row.set("variation_consequence_count", count);
     }
 
-    /*
-
-      Ensembl REST feature endpoint doesn't provide proper variation feature type , all feature_type are 'variant'
-
-    private void addFeatureCounts(final CyNode node, final CyNetwork network, final List<Variation> variations)
+    private void addConsequenceCounts(final CyNode node, final CyNetwork network, final List<VariationConsequence> variationConsequences)
     {
-        Domain sf = sequenceFeatures();
-        Map<String, Concept> indexByName = indexByName(sf);
+        Domain sv = sequenceVariants();
+        Map<String, Concept> indexByName = indexByName(sv);
 
-        Authority so = sf.getAuthority();
+        Authority so = sv.getAuthority();
         Assignable assignableNode = new AssignableNode(node);
         Set<Evidence> evidence = ImmutableSet.of(new Evidence("IEA", 1.0d, 1.0d));
-        for (Variation variation : variations)
+        for (VariationConsequence variationConsequence : variationConsequences)
         {
-            so.createAssignment(indexByName.get(variation.getFeatureType()), assignableNode, evidence);
+            so.createAssignment(indexByName.get(variationConsequence.getSequenceOntologyTerm()), assignableNode, evidence);
         }
 
         CyTable table = network.getDefaultNodeTable();
         CyRow row = table.getRow(node.getSUID());
-        for (Map.Entry<Concept, Integer> entry : countAssignments(domain))
+        for (Map.Entry<Concept, Integer> entry : countAssignments(sv).entrySet())
         {
             Concept concept = entry.getKey();
             Integer count = entry.getValue();
@@ -178,7 +212,6 @@ final class AnnotateKnownVariationsTask
             row.set(concept.getName(), count);
         }
     }
-    */
 
     private void slowDown()
     {
@@ -190,6 +223,17 @@ final class AnnotateKnownVariationsTask
         catch (InterruptedException e)
         {
             // ignore
+        }
+    }
+
+    private static final class AssignableNode extends AbstractAssignable
+    {
+        private final CyNode node;
+
+        AssignableNode(final CyNode node)
+        {
+            super();
+            this.node = node;
         }
     }
 }
