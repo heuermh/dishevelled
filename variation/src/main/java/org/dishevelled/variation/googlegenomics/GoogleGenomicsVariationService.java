27cd4bb93b3c38f76d5a6939eade7230e42476e2
heuermh <heuermh@acm.org>
2014-06-04 22:37:04 +0000

    adding Google Genomics API variation service

---
+++ variation/src/main/java/org/dishevelled/variation/googlegenomics/GoogleGenomicsVariationService.java
@@ -0,0 +1,125 @@
+/*
+
+    dsh-variation  Variation.
+    Copyright (c) 2013-2014 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.variation.googlegenomics;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.google.api.services.genomics.Genomics;
+
+import com.google.api.services.genomics.model.SearchVariantsRequest;
+import com.google.api.services.genomics.model.SearchVariantsResponse;
+import com.google.api.services.genomics.model.Variant;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
+
+import org.dishevelled.variation.Feature;
+import org.dishevelled.variation.Variation;
+import org.dishevelled.variation.VariationService;
+
+/**
+ * Google Genomics API variation service.
+ */
+public final class GoogleGenomicsVariationService implements VariationService
+{
+    /** Species. */
+    private final String species;
+
+    /** Reference. */
+    private final String reference;
+
+    /** DatasetId. */
+    private final String datasetId;
+
+    /** Google Genomics API. */
+    private final Genomics genomics;
+
+
+    /**
+     * Create a new Google Genomics API variation service.
+     *
+     * @param species species, must not be null
+     * @param reference reference, must not be null
+     * @param datasetId datasetId, must not ben null
+     * @param genomics Google Genomics API, must not be null
+     */
+    public GoogleGenomicsVariationService(final String species,
+                                          final String reference,
+                                          final String datasetId,
+                                          final Genomics genomics)
+    {
+        checkNotNull(species);
+        checkNotNull(reference);
+        checkNotNull(datasetId);
+        checkNotNull(genomics);
+        this.species = species;
+        this.reference = reference;
+        this.datasetId = datasetId;
+        this.genomics = genomics;
+    }
+
+
+    @Override
+    public List<Variation> variations(final Feature feature)
+    {
+        checkNotNull(feature);
+        checkArgument(species.equals(feature.getSpecies()));
+        checkArgument(reference.equals(feature.getReference()));
+
+        SearchVariantsRequest request = new SearchVariantsRequest()
+            .setDatasetId(datasetId)
+            .setContig(feature.getRegion())
+            .setStartPosition(Long.valueOf(feature.getStart()))
+            .setEndPosition(Long.valueOf(feature.getEnd()));
+        
+        List<Variation> variations = Lists.newArrayList();
+        try
+        {
+            SearchVariantsResponse response = genomics.variants().search(request).execute();
+
+            for (Variant variant : response.getVariants())
+            {
+                List<String> identifiers = ImmutableList.copyOf(variant.getNames());
+                String ref = variant.getReferenceBases();
+                List<String> alt = ImmutableList.copyOf(variant.getAlternateBases());
+                String region = variant.getContig();
+                int position = variant.getPosition().intValue(); // todo: variant position is 1-based, confirm this is correct
+                int start = position - 1;
+                int end = start + ref.length();
+                variations.add(new Variation(species, reference, identifiers, ref, alt, region, start, end));
+            }
+        }
+        catch (IOException e)
+        {
+            // todo
+        }
+        return variations;
+    }
+}
